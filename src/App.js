41b024b172e5ca897183760790a3ddefe89d4c7a
nascjoao <oi@joaonasc.dev>
2021-05-03 16:16:32 -0300

    Renderiza MovieLibrary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
+import MovieLibrary from './components/MovieLibrary';
+import moviesData from './data';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieLibrary movies={ moviesData } />
     </div>
   );
 }
