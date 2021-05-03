7f8495a79b937046a8744334a1cfaedf8298cde0
Hernanne <hernannedomiciano@gmail.com>
2021-05-03 16:37:24 -0300

    Requisitos SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,62 @@
-// implement AddMovie component here
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label">
+          Inclui o texto
+          <input
+            data-testid="text-input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+        <label data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
+        <label data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            data-testid="select-input"
+            name="selectedGenre"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option value="" data-testid="select-option"> Todos </option>
+            <option value="action" data-testid="select-option"> Ação </option>
+            <option value="comedy" data-testid="select-option"> Comédia </option>
+            <option value="thriller" data-testid="select-option"> Suspense </option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
+  selectedGenre: PropTypes.bool,
+  onSelectedGenreChange: PropTypes.func,
+}.isRequired;
