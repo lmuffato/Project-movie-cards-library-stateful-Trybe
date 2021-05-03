3e3c4066ecf140d44729db70bdf40ac2e5017fd4
Luiz Felipe Araujo <thelastnailmusic@gmail.com>
2021-05-03 19:10:31 -0300

    8 a 16 em andamento

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,49 +1,48 @@
 // implement AddMovie component here
 import React from 'react';
-import propTypes from 'prop-types';
+// import propTypes from 'prop-types';
+import MovieTitle from './MovieComponents/MovieTitle';
+import MovieSubtitle from './MovieComponents/MovieSubtitle';
+import MovieImage from './MovieComponents/MovieImage';
+import MovieStoryLine from './MovieComponents/MovieStoryline';
+import MovieRating from './MovieComponents/MovieRating';
+import MovieGenre from './MovieComponents/MovieGenre';
+import MovieButtonAdd from './MovieComponents/MovieButtonAdd';
 
 class AddMovie extends React.Component {
-  constructor() {
-    super();
-    // this.onClick = this.onClick.bind(this);
+  // constructor() {
+  //   super();
 
-    this.state = {
-      title: '',
-    };
-
-    // this.state = {
-    //   subtitle: '',
-    //   title: '',
-    //   imagePath: '',
-    //   storyLine: '',
-    //   rating: 0,
-    //   genre: 'action',
-    // };
-  }
+  //   this.state = {
+  //     subtitle: '',
+  //     title: '',
+  //     imagePath: '',
+  //     storyLine: '',
+  //     rating: 0,
+  //     genre: 'action',
+  //   };
+  // }
 
   render() {
-    const { onClick } = this.props;
-    const { title } = this.state;
+    // const { onClick } = this.props;
     // const { title, subtitle, imagePath, storyLine, rating, genre } = this.state;
 
     return (
       <form data-testid="add-movie-form">
-        <label htmlFor="title-input" data-testid="title-input-label">
-          Título
-          <input
-            name="title-input"
-            value={ title }
-            data-testid="title-input"
-            onChange={ onClick }
-          />
-        </label>
+        <MovieTitle />
+        <MovieSubtitle />
+        <MovieImage />
+        <MovieStoryLine />
+        <MovieRating />
+        <MovieGenre />
+        <MovieButtonAdd />
       </form>
     );
   }
 }
 
-AddMovie.propTypes = {
-  onClick: propTypes.func.isRequired,
-};
+// AddMovie.propTypes = {
+//   onClick: propTypes.func.isRequired,
+// };
 
 export default AddMovie;
