c0a8f7704fbfa8a78ea0dba3c8079b45ba40599f
Iago de Paula <iago.pferreiravr@gmail.com>
2021-05-03 15:27:55 -0300

    Requisito 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    return ('');
+    return (
+      <form data-testid="search-bar-form"></form>
+    );
   }
 }
+
+SearchBar.propTypes = {};
 
 export default SearchBar;
