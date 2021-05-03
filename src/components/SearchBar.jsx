6b200450f89df77561d133387dc6d39b2b30b113
Rodrigo Rech <rodrigorech92@gmail.com>
2021-05-03 17:17:48 -0300

    text Input created at forms in SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
+    const { searchText, onSearchTextChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <h1>a</h1>
+        <label data-testid="text-input-label" htmlFor="text-input">
+          Inclui o texto
+          <input
+            data-testid="text-input"
+            type="text"
+            onChange={ onSearchTextChange }
+            value={ searchText }
+            id="text-input"
+          />
+        </label>
+
       </form>
     );
   }
 }
 
 export default SearchBar;
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+};
