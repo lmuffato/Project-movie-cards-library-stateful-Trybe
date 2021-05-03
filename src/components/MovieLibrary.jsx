bfff679bde8932973a26aceccab64a860ec6a2a4
Giovanni <giovannitadeu08@gmail.com>
2021-05-03 15:01:15 -0300

    [Lint fix]

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,24 +1,25 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import MovieList from "./MovieList";
-import SearchBar from "./SearchBar";
-import AddMovie from "./AddMovie";
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-    constructor(props) {
-        super(props);
-    }
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div>
-                <h2> My awesome movie library </h2>
-                <SearchBar />
-                <MovieList movies={this.props.movies} />
-                <AddMovie />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={this.props.movies} />
+        <AddMovie />
+      </div>
+    );
+  }
 }
 
 export default MovieLibrary;
