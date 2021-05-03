11fac7a6974c94b46a64ae7194586981551f72db
Bruno Mendes <mendessbruno@gmail.com>
2021-05-03 16:37:17 -0300

    resolvido ate o requisito 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Header from './components/Header';
-import SearchBar from './components/SearchBar'
+import SearchBar from './components/SearchBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar searchText='' onSearchTextChange={'clallBack'} bookmarkedOnly='bool' onBookmarkedChange='callback' selectedGenre='' onSelectedGenreChange='callback' />
+      <SearchBar 
+        searchText='string' 
+        onSearchTextChange={'clallBack'} 
+        bookmarkedOnly='bool' 
+        onBookmarkedChange='callback' 
+        selectedGenre='string' 
+        onSelectedGenreChange='callback' />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 
-class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-  }
-    
+class SearchBar extends Component {   
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    const { 
+      searchText, 
+      onSearchTextChange, 
+      // bookmarkedOnly, 
+      // onBookmarkedChange, 
+      // selectedGenre, 
+      // onSelectedGenreChange 
+    } = this.props;
 
     return (
-      <form>
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label">"Inclui o texto:"
+        <input type="text" value={searchText} onChange={onSearchTextChange} data-testid="text-input"></input>
+        </label>
 
       </form>
     );
