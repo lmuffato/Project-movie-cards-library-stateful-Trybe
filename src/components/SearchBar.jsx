25220289a85dec9a3e85178831226c7c47f026f6
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-05-03 17:31:52 -0300

    Adicionando requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,37 +3,45 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    // onSearchTextChange, bookmarkedOnly, selectedGenre, onSelectedGenreChange
-    const { searchText,
-      onSearchTextChange,
-      onBookmarkedChange,
-      bookmarkedOnly } = this.props;
+    const { searchText, onSearchTextChange, onBookmarkedChange, bookmarkedOnly,
+      selectedGenre, onSelectedGenreChange } = this.props;
+
     return (
       <form data-testid="search-bar-form">
-        <div>
-          <label htmlFor="search-text" data-testid="text-input-label">
-            Inclui o texto
-            <input
-              id="search-text"
-              data-testid="text-input"
-              type="text"
-              value={ searchText }
-              onChange={ onSearchTextChange }
-            />
-          </label>
-        </div>
-        <div>
-          <label htmlFor="search-checkbox" data-testid="checkbox-input-label">
-            Mostrar somente favoritos
-            <input
-              id="search-checkbox"
-              data-testid="checkbox-input"
-              type="checkBox"
-              onChange={ onBookmarkedChange }
-              checked={ bookmarkedOnly }
-            />
-          </label>
-        </div>
+        <label htmlFor="search-text" data-testid="text-input-label">
+          Inclui o texto
+          <input
+            id="search-text"
+            data-testid="text-input"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+        <label htmlFor="search-checkbox" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            id="search-checkbox"
+            data-testid="checkbox-input"
+            type="checkBox"
+            onChange={ onBookmarkedChange }
+            checked={ bookmarkedOnly }
+          />
+        </label>
+        <label htmlFor="search-select" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            id="search-select"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
@@ -44,6 +52,8 @@ SearchBar.propTypes = {
   onSearchTextChange: PropTypes.func.isRequired,
   onBookmarkedChange: PropTypes.func.isRequired,
   bookmarkedOnly: PropTypes.bool.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
