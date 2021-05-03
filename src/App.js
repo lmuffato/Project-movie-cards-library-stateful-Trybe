e1992919e15bd4247564cdfadbfe8596daa60b4f
AdelinoJnr <adelinojunior96@gmail.com>
2021-05-03 15:32:51 -0300

    Requisito 6 Crie um componente chamado AddMovie

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
       onSelectedGenreChange={console.log}
       />
       <MovieList movies={data} />
+      
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,14 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  render() {
+    return (
+      <div>
+        <p>Ola</p>
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
