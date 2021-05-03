93db71ace09d40724bfda4014d25d6e515886125
Fernando Resende <fernandocastellob@gmail.com>
2021-05-03 15:12:31 -0300

    adicionado codigo no movielibrary(conforme descricao do projeto)

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,20 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={ this.props.movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
