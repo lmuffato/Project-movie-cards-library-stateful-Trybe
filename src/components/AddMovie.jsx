fd6c7668bd75321a168bf1e03d50ddc3ee9f0ef5
Vinicius Rodrigues <v.rodriguesjk@gmail.com>
2021-05-03 20:20:10 -0300

    Requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,13 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" />
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
