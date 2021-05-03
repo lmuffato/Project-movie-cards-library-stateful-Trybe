486a63da46ca595b13b0f0a6fc942fbba4f38daf
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-05-03 17:10:48 -0300

    Adicionando requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -31,6 +31,19 @@ class SearchBar extends Component {
             data-testid="checkbox-input"
           />
         </label>
+        <label htmlFor="select-input" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
