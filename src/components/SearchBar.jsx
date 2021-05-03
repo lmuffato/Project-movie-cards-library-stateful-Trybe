13bb4df860d1f344bae99bc1b309b75a5562583c
MaxRudim <max.hermsdorff@gmail.com>
2021-05-03 18:29:01 -0300

    Até o req. 05 feito

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,14 +8,46 @@ class SearchBar extends Component {
     return (
       <div>
         <form data-testid="search-bar-form">
-          <label data-testid="text-input-label">Inclui o texto</label>
-          <input
-            type="text"
-            data-testid="text-input"
-            value="searchText"
-            onChange="onSearchTextChange"
-          />
-          <label data-testid="checkbox-input-label">Mostrar somente favoritos</label>
+          <label htmlFor="label1" data-testid="text-input-label">
+            Inclui o texto:
+            <input
+              type="text"
+              id="label1"
+              data-testid="text-input"
+              value={ searchText }
+              onChange={ onSearchTextChange }
+            />
+          </label>
+          <label
+            htmlFor="label2"
+            data-testid="checkbox-input-label"
+          >
+            Mostrar somente favoritos
+            <input
+              type="checkbox"
+              id="label2"
+              data-testid="checkbox-input"
+              checked={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
+            />
+          </label>
+          <label
+            htmlFor="label3"
+            data-testid="select-input-label"
+          >
+            Filtrar por gênero
+            <select
+              id="label3"
+              data-testid="select-input"
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+            >
+              <option value="" data-testid="select-option">Todos</option>
+              <option value="action" data-testid="select-option">Ação</option>
+              <option value="comedy" data-testid="select-option">Comédia</option>
+              <option value="thriller" data-testid="select-option">Suspense</option>
+            </select>
+          </label>
         </form>
       </div>
     );
