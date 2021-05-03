0558f6e921df3cc603f929ccb87fe812e6ac5e22
Marilia Cegalla Aldrighi <maricegalla@gmail.com>
2021-05-03 18:53:32 -0300

    Requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,19 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  render() {
+    const { onClick } = this.props;
+    return (
+      <div value={ onClick }>
+        <form data-testid="add-movie-form" />
+      </div>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
