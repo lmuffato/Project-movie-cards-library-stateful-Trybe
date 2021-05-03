687e8780c10c235e64f1427fef60f7e8d26220d9
Lucas André <lucas.cancado89@gmail.com>
2021-05-03 17:47:06 -0300

    Requisito 12

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -8,55 +8,99 @@ class AddMovie extends React.Component {
       title: '',
       imagePath: '',
       storyline: '',
-      // rating: 0,
+      rating: 0,
       // genre: 'action',
     };
   }
 
+  addtitle = () => {
+    const { title } = this.state;
+    return (
+      <label htmlFor="titulo" data-testid="title-input-label">
+        Título
+        <input
+          type="text"
+          id="titulo"
+          data-testid="title-input"
+          value={ title }
+          onChange={ (event) => this.setState({ title: event.target.value }) }
+        />
+      </label>
+    );
+  }
+
+  addSubtitle = () => {
+    const { subtitle } = this.state;
+    return (
+      <label htmlFor="subTitulo" data-testid="subtitle-input-label">
+        Subtítulo
+        <input
+          type="text"
+          id="subTitulo"
+          data-testid="subtitle-input"
+          value={ subtitle }
+          onChange={ (event) => this.setState({ subtitle: event.target.value }) }
+        />
+      </label>
+    );
+  }
+
+  addImage = () => {
+    const { imagePath } = this.state;
+    return (
+      <label htmlFor="image" data-testid="image-input-label">
+        Subtítulo
+        <input
+          type="text"
+          id="image"
+          data-testid="image-input"
+          value={ imagePath }
+          onChange={ (event) => this.setState({ imagePath: event.target.value }) }
+        />
+      </label>
+    );
+  }
+
+  addSinopse = () => {
+    const { storyline } = this.state;
+    return (
+      <label htmlFor="sinopse" data-testid="storyline-input-label">
+        Sinopse
+        <textarea
+          type="text"
+          id="sinopse"
+          data-testid="storyline-input"
+          value={ storyline }
+          onChange={ (event) => this.setState({ storyline: event.target.value }) }
+        />
+      </label>
+    );
+  }
+
+  addRating = () => {
+    const { rating } = this.state;
+    return (
+      <label htmlFor="rating" data-testid="rating-input-label">
+        Avaliação
+        <input
+          type="number"
+          id="rating"
+          data-testid="rating-input"
+          value={ rating }
+          onChange={ (event) => this.setState({ rating: event.target.value }) }
+        />
+      </label>
+    );
+  }
+
   render() {
-    const { title, subtitle, imagePath, storyline } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label htmlFor="titulo" data-testid="title-input-label">
-          Título
-          <input
-            type="text"
-            id="titulo"
-            data-testid="title-input"
-            value={ title }
-            onChange={ (event) => this.setState({ title: event.target.value }) }
-          />
-        </label>
-        <label htmlFor="subTitulo" data-testid="subtitle-input-label">
-          Subtítulo
-          <input
-            type="text"
-            id="subTitulo"
-            data-testid="subtitle-input"
-            value={ subtitle }
-            onChange={ (event) => this.setState({ subtitle: event.target.value }) }
-          />
-        </label>
-        <label htmlFor="image" data-testid="image-input-label">
-          Subtítulo
-          <input
-            type="text"
-            id="image"
-            data-testid="image-input"
-            value={ imagePath }
-            onChange={ (event) => this.setState({ imagePath: event.target.value }) }
-          />
-        </label>
-        <label htmlFor="sinopse" data-testid="storyline-input-label">
-          Sinopse
-          <textarea
-            type="text"
-            id="sinopse"
-            data-testid="storyline-input"
-            value={ storyline }
-            onChange={ (event) => this.setState({ storyline: event.target.value }) }
-          />
-        </label>
+        {this.addtitle()}
+        {this.addSubtitle()}
+        {this.addImage()}
+        {this.addSinopse()}
+        {this.addRating()}
       </form>
     );
   }
