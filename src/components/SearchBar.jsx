9858f75f05065d0e5748999a4d089a5864d3be09
Guilherme de Prais <guilherme.prais@gmail.com>
2021-05-03 17:00:26 -0300

    requirement 05

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -23,6 +23,17 @@ function SearchBar({
         onChange={ onBookmarkedChange }
         data-testid="checkbox-input"
       />
+      <label data-testid="select-input-label">Filtrar por gênero</label>
+      <select
+        value={ selectedGenre }
+        onChange={ onSelectedGenreChange }
+        data-testid="select-input"
+      >
+        <option value="" data-testid="select-option">Todos</option>
+        <option value="action" data-testid="select-option">Ação</option>
+        <option value="comedy" data-testid="select-option">Comédia</option>
+        <option value="thriller" data-testid="select-option">Suspense</option>
+      </select>
     </form>
   );
 }
