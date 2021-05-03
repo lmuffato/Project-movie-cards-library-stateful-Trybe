c7ff1110cec23b150c985e5a280bcddd25086573
madruga665 <madruga665@hotmail.com>
2021-05-03 17:33:40 -0300

    remove input-group e add select

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,51 +1,66 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    // const {
-    //   // searchText,
-    //   // onSearchTextChange,
-    //   // bookmarkedOnly,
-    //   // onBookmarkedChange,
-    //   // selectedGenre,
-    //   // onSelectedGenreChange,
-    // } = this.props;
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
     return (
-      <div>
-        <form data-testid="search-bar-form">
-          <div className="input-group">
-            <label htmlFor="search-input" data-testid="text-input-label">
-              Inclui o texto:
-              <input
-                id="search-input"
-                type="text"
-                value="searchText"
-                onChange="onSearchTextChange"
-                data-testid="text-input"
-              />
-            </label>
-          </div>
+      <form data-testid="search-bar-form">
 
-          <div className="input-group">
-            <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
-              Mostrar somente favoritos
-              <input
-                id="checkbox-input"
-                type="checbox"
-                onChange="onBookmarkedChange"
-                data-testid="checkbox-input"
-              />
-            </label>
-          </div>
-        </form>
-      </div>
+        <label htmlFor="search-input" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            id="search-input"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+
+        <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            id="checkbox-input"
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
+
+        <label htmlFor="select-input" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            id="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
+      </form>
     );
   }
 }
 
-// SearchBar.prototype = {
-//   searchText: PropTypes.string,
-// };
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
