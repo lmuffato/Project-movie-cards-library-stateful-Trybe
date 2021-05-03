21de16f3875a44b988d8e97882642bc8f565d8cc
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-05-03 16:50:19 -0300

    Adicionando requisitos 2 e 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
     </div>
   );
 }
-
-// pro pr inicial
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,23 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const { searchText, onSearchTextChange } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            name="nome"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
