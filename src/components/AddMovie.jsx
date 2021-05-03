c04e20900db4bd820d1516b56286198bfb2694a9
Ana Ventura <anaventura1811@gmail.com>
2021-05-03 16:42:28 -0300

    Adiciona requisitos 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,36 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
 // implement AddMovie component here
+class AddMovie extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     subtitle: '',
+  //     title: '',
+  //     imagePath: '',
+  //     storyline: '',
+  //     rating: '',
+  //     genre: '',
+  //   };
+  // }
+  render() {
+    // const { subtitle, title, imagePath, storyline, rating, genre } = this.props;
+    return (
+      <form data-testid="add-movie-form">
+        <input type="text" />
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
+
+// AddMovie.propTypes = {
+//   subtitle: PropTypes.string.isRequired,
+//   title: PropTypes.string.isRequired,
+//   imagePath: PropTypes.string.isRequired,
+//   storyline: PropTypes.string.isRequired,
+//   rating: PropTypes.number.isRequired,
+//   genre: PropTypes.string.isRequired,
+// };
