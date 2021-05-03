d06ac0dfceabf54c343feab1f39ffa0a5648dda4
Patrick Morais <ppternunes@gmail.com>
2021-05-03 17:39:32 -0300

    requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,13 +12,31 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+    this.handleTitle = this.handleTitle.bind(this);
+  }
+
+  handleTitle(event) {
+    const { name, value } = event.target;
+    this.setState = {
+      [name]: value,
+    };
   }
 
   render() {
     const { onClick } = this.props;
+    const { title } = this.state;
     return (
       <form data-testid="add-movie-form">
-
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            name="title"
+            type="text"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleTitle }
+          />
+        </label>
       </form>
     );
   }
