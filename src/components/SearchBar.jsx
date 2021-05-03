489bc8d171f8e82481e91ba390c1c9eadfb85262
Willian Mendes <willianmendesbr@gmail.com>
2021-05-03 19:26:04 -0300

    select com opções de generos de filmes criado - Requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -34,6 +34,20 @@ class SearchBar extends Component {
             data-testid="checkbox-input"
           />
         </label>
+        <label htmlFor="select-input" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            id="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
