3a3d93aadcbe0267bb594d702239488630319dab
jonathan <jonathanfernandesjf98@gmail.com>
2021-05-03 18:26:01 -0300

    requisitos 06 e 07

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,28 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action'
+    };
+  }
+
+  render() {
+    return (
+      <form
+        data-testid="add-movie-form"
+      >
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
