e22a581b2cc4d478fbcb508004ed59da4faad8d8
Mateus Alencar <mtauws@gmail.com>
2021-05-03 16:10:02 -0300

    chore: requirement 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    // const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
-      <form data-testid="search-bar-form" />
+      <form data-testid="search-bar-form">
+          <label htmlFor="search-text" data-testid="text-input-label" >Inclui o texto:
+          <input 
+              id="search-text"
+              value={ searchText }
+              onChange={ onSearchTextChange }
+              data-testid="text-input"
+            /></label>
+
+      </form>
     );
   }
 }
