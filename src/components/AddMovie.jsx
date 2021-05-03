8886910c452bbec9cb09ee88579c19bc627b5ddc
madruga665 <madruga665@hotmail.com>
2021-05-03 18:53:40 -0300

    Requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      // subtitle: '',
+      // imagePath: '',
+      // storyline: '',
+      // rating: 0,
+      // genre: 'action',
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ title: event.target.value });
+  }
+
   render() {
+    const { title } = this.state;
     return (
-      <form>
-        <h1>aqui vai o forms pra add novo filme :) </h1>
+      <form data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            id="title-input"
+            type="text"
+            value={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
       </form>
     );
   }
