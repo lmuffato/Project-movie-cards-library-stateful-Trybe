1a72ad3884805e3dd9fae67a3e72f95cd0596740
nascjoao <oi@joaonasc.dev>
2021-05-03 14:37:14 -0300

    ✔ Requisito 1: Crie um componente chamado <SearchBar />

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,12 @@
-// implement AddMovie component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+function SearchBar() {
+  return (
+    <div />
+  );
+}
+
+SearchBar.propTypes = {};
+
+export default SearchBar;
