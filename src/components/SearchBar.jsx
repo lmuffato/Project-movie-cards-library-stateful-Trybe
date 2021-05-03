1ce6d94277803e7d6eabbc16ef947d266fd9d305
Mateus Alencar <mtauws@gmail.com>
2021-05-03 15:41:27 -0300

    correction: lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,11 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <h1>SerachBox</h1>
+    );
+  }
+}
+
+export default SearchBar;
