22b7936f571b7a2646316f1231f9c0f6a5404cb0
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-05-03 16:43:53 -0300

    Adicionando requisitos 1 e 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    // onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange
+    const { searchText, onSearchTextChange } = this.props;
     return (
-      <div>
-        Filtrar por gênero
-        <select>
-          <option>Ação</option>
-        </select>
-      </div>
+      <form data-testid="search-bar-form">
+        <div >
+          <label htmlFor="search" data-testid="text-input-label">
+            Inclui o texto
+            <input
+              id="search"
+              data-testid="text-input"
+              type="text"
+              value={ searchText }
+              onChange={ onSearchTextChange }
+            />
+          </label>
+        </div>
+      </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
