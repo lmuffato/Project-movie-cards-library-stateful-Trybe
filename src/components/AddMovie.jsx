0dcf1796b0c7578be3d58a3185a4a2dd5ee076a6
Gabriel Ferreira <glucas709@gmail.com>
2021-05-03 18:56:48 -0300

    Requisito 11

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Inputs from './InputsText';
 
 export default class AddMovie extends React.Component {
   constructor() {
@@ -7,7 +8,7 @@ export default class AddMovie extends React.Component {
       subtitle: '',
       title: '',
       imagePath: '',
-      // storyline: '',
+      storyline: '',
       // rating: 0,
       // genre: 'action',
     };
@@ -19,44 +20,29 @@ export default class AddMovie extends React.Component {
     this.setState({
       [name]: value,
     });
-  }
+  };
 
   render() {
-    const { title, subtitle, imagePath } = this.state;
+    const { title, subtitle, imagePath, storyline } = this.state;
     console.log(subtitle);
 
     return (
       <form action="" data-testid="add-movie-form">
-        <label htmlFor="title-input" data-testid="title-input-label">
-          Título
-          <input
-            type="text"
-            name="title"
-            id="title-input"
-            data-testid="title-input"
-            value={ title }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
-          Subtítulo
-          <input
-            type="text"
-            name="subtitle"
-            id="subtitle-input"
-            data-testid="subtitle-input"
-            value={ subtitle }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="image-input" data-testid="image-input-label">
-          Imagem
-          <input
-            type="text"
-            name="imagePath"
-            id="image-input"
-            data-testid="image-input"
-            value={ imagePath }
+        <Inputs
+          title={ title }
+          subtitle={ subtitle }
+          imagePath={ imagePath }
+          handleChange={ this.handleChange }
+        />
+        <label htmlFor="storyline-input" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            name="storyline"
+            id="storyline-input"
+            data-testid="storyline-input"
+            cols="30"
+            rows="10"
+            value={ storyline }
             onChange={ this.handleChange }
           />
         </label>
---
+++ src/components/InputsText.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Inputs extends React.Component {
+  render() {
+    const { title, subtitle, imagePath, handleChange } = this.props;
+    return (
+      <>
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            name="title"
+            id="title-input"
+            data-testid="title-input"
+            value={ title }
+            onChange={ handleChange }
+          />
+        </label>
+        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            name="subtitle"
+            id="subtitle-input"
+            data-testid="subtitle-input"
+            value={ subtitle }
+            onChange={ handleChange }
+          />
+        </label>
+        <label htmlFor="image-input" data-testid="image-input-label">
+          Imagem
+          <input
+            type="text"
+            name="imagePath"
+            id="image-input"
+            data-testid="image-input"
+            value={ imagePath }
+            onChange={ handleChange }
+          />
+        </label>
+      </>
+    );
+  }
+}
+
+Inputs.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  imagePath: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
