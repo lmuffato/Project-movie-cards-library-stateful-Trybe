bffabfba3ff8555520b5aeb418cad4c7fd0c7231
Iago de Paula <iago.pferreiravr@gmail.com>
2021-05-03 15:55:35 -0300

    Requisito 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -16,7 +16,9 @@ class AddMovie extends React.Component {
 
   render() {
     const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
-    return ('');
+    return (
+      <form data-testid="add-movie-form"></form>
+    );
   }
 }
 export default AddMovie;
