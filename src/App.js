7344adf68a2da23b06ec07ff223fa7f3625b715a
Alexandre Oliveira <alexandremolive@gmail.com>
2021-05-03 15:16:34 -0300

    primeiro requisito

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 import movies from './data';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar
+        searchText={ searchText }
+        onSearchTextChange={ onSearchTextChange }
+        bookmarkedOnly={ bookmarkedOnly }
+        onBookmarkedChange={ onBookmarkedChange }
+        selectedGenre={ selectedGenre }
+        onSelectedGenreChange={ onSelectedGenreChange }
+      />
       <MovieList movies={ movies } />
     </div>
   );
