621b81018574cc17c3c16a7c5f77e7cd1a1f57b9
Vinicius Rodrigues <v.rodriguesjk@gmail.com>
2021-05-03 19:28:28 -0300

    Requisito 4 e 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,18 +3,49 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
-    const { onSearchTextChange, searchText } = this.props;
+    const {
+      onSearchTextChange,
+      searchText,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label" htmlFor="myInput">
+        <label data-testid="text-input-label" htmlFor="myInputText">
           Inclui o texto:
           <input
-            id="myInput"
+            id="myInputText"
             onChange={ onSearchTextChange }
             type="text"
             value={ searchText }
             data-testid="text-input"
           />
+        </label>
+        <label data-testid="checkbox-input-label" htmlFor="myInputCheckbox">
+          Mostrar somente favoritos:
+          <input
+            id="myInputCheckbox"
+            onChange={ onBookmarkedChange }
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            data-testid="checkbox-input"
+          />
+        </label>
+        <label data-testid="select-input-label" htmlFor="mySelect">
+          Filtrar por gênero:
+          <select
+            id="mySelect"
+            onChange={ onSelectedGenreChange }
+            value={ selectedGenre }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
         </label>
       </form>
     );
@@ -24,6 +55,10 @@ class SearchBar extends Component {
 SearchBar.propTypes = {
   onSearchTextChange: PropTypes.func.isRequired,
   searchText: PropTypes.string.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
