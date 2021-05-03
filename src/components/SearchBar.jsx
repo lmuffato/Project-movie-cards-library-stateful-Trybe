a650a68e3840e585e78b0d348c68118c116c2b82
Mateus Alencar <mtauws@gmail.com>
2021-05-03 16:28:21 -0300

    chore: requirement 3:lint and props

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
-    /* bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange */
+    // , bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange
     const { searchText, onSearchTextChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-          <label htmlFor="search-text" data-testid="text-input-label" >Inclui o texto:</label>
-          <input 
+        <div className="form-group">
+          <label htmlFor="search-text" data-testid="text-input-label">
+            Inclui o texto:
+            <input
               id="search-text"
               value={ searchText }
               onChange={ onSearchTextChange }
               data-testid="text-input"
             />
-
+          </label>
+        </div>
       </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  // bookmarkedOnly: PropTypes.bool,
+  // onBookmarkedChange: PropTypes.func,
+  // selectedGenre: PropTypes.string,
+  // onSelectedGenreChange: PropTypes.func,
+}.isRequired;
 
 export default SearchBar;
