75039c83f6c873dfb81acd2bcca43bc92a9c67bf
Rodrigo Rech <rodrigorech92@gmail.com>
2021-05-03 15:35:01 -0300

    SearchBar component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar
+        searchText="string"
+        onSearchTextChange={ useCallback() }
+        bookmarkedOnly={ false }
+        onBookmarkedChange={ useCallback() }
+        selectedGenre="string"
+        onSelectedGenreChange={ useCallback() }
+      />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,9 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (<h1> aaa</h1>);
+  }
+}
+
+export default SearchBar;
