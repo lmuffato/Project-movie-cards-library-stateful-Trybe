09b31455f4a8ed6dcbfd0464579659e3aceebd40
Rodrigo Rech <rodrigorech92@gmail.com>
2021-05-03 18:53:13 -0300

    Form implemented at AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -17,14 +17,17 @@ class AddMovie extends Component {
   render() {
     const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
-      <h1>
-        { subtitle }
-        { title }
-        { imagePath }
-        { storyline }
-        { rating }
-        { genre }
-      </h1>
+      <form action="" data-testid="add-movie-form">
+        <h1>
+          {subtitle}
+          {title}
+          {imagePath}
+          {storyline}
+          {rating}
+          {genre}
+        </h1>
+
+      </form>
     );
   }
 }
