57b2e525433299bf8bd3df91180d2127f2932bbc
AdelinoJnr <adelinojunior96@gmail.com>
2021-05-03 15:09:08 -0300

    Requisito 1 Crie um componente chamado SearchBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import SearchBar from './components/SearchBar';
+import data from './data'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
+      <MovieList movies={data} />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,14 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div>
+        <input type="text" placeholder="Escolha Filho" />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
