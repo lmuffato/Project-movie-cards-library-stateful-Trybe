8c1f7e29a23099fa0dbf5f97985c4b587c0c8aac
Mateus Alencar <mtauws@gmail.com>
2021-05-03 16:12:29 -0300

    chore: requirement 3:lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    /* bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange */
+    const { searchText, onSearchTextChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-          <label htmlFor="search-text" data-testid="text-input-label" >Inclui o texto:
+          <label htmlFor="search-text" data-testid="text-input-label" >Inclui o texto:</label>
           <input 
               id="search-text"
               value={ searchText }
               onChange={ onSearchTextChange }
               data-testid="text-input"
-            /></label>
+            />
 
       </form>
     );
