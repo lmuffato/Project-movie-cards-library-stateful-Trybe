28f95110535ea471fd6c273d4e09e23d92a44653
Gustavo Sousa <gustavohl98@gmail.com>
2021-05-03 18:59:39 -0300

    Requisito 03 - renderiza input dentro do form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 import MovieList from './components/MovieList';
 import movies from './data';
 import './App.css';
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
       <MovieList movies={ movies } />
     </div>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const { searchText, onSearchTextChange } = this.props;
+
     return (
-      <form data-testid="search-bar-form" />
+      <form data-testid="search-bar-form">
+        <div>
+          <label
+            htmlFor="text-input-1"
+            data-testid="text-input-label"
+          >
+            Inclui o texto:
+            <input
+              type="text"
+              value={ searchText }
+              onChange={ onSearchTextChange }
+              data-testid="text-input"
+            />
+          </label>
+        </div>
+      </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
