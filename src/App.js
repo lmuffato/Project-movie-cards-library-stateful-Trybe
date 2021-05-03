9eaceec4c48f068498001d4fe602cc685fa85e41
Luiz Felipe Araujo <thelastnailmusic@gmail.com>
2021-05-03 16:24:06 -0300

    1 a 4 OK

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import MovieLibrary from './components/MovieLibrary';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,11 +3,40 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-
   render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange } = this.props;
+
     return (
       <form data-testid="search-bar-form">
-        <div>Oi</div>
+        <label
+          id="text-input-label"
+          htmlFor="inputSearchBar"
+          data-testid="text-input-label"
+        >
+          Inclui o texto
+          <input
+            name="inputSearchBar"
+            type="text"
+            data-testid="text-input"
+            value={searchText}
+            onChange={onSearchTextChange}
+          />
+        </label>
+        <label htmlFor="" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+            data-testid="checkbox-input"
+          />
+        </label>
       </form>
     );
   }
