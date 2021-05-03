763be2e8d87edfdd42d968fd22d7f86199866a6c
Alexandre Oliveira <alexandremolive@gmail.com>
2021-05-03 15:00:09 -0300

    organizando as pastas ja criadas

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,12 @@
 // implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <h1>testando</h1>
+    );
+  }
+}
+
+export default SearchBar;
