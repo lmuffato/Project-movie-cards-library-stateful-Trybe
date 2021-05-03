9fb1e14ab0c2be46bc8e6c52eda937c227656ad0
Mateus Alencar <mtauws@gmail.com>
2021-05-03 18:25:07 -0300

    chore: requirement 12 - 14

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,16 +4,33 @@ class AddMovie extends Component {
   constructor() {
     super();
 
+    this.AddMovie = this.AddMovie.bind(this);
     this.state = {
       title: '',
       subtitle: '',
       imagePath: '',
       storyline: '',
+      rating: 0,
+      genre: 'action',
     };
   }
 
+  // Source: https://github.com/tryber/sd-010-a-project-movie-cards-library-stateful/tree/b6df1691507cba7f278c15b8dd06ccb0cf0281e6
+  AddMovie() {
+    const { onClick } = this.props;
+    onClick(this.state);
+    this.setState({
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
   render() {
-    const { title, subtitle, imagePath, storyline } = this.state;
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="title-input" data-testid="title-input-label">
@@ -57,6 +74,30 @@ class AddMovie extends Component {
             onChange={ (event) => this.setState({ storyline: event.target.value }) }
           />
         </label>
+        <label htmlFor="rating-input" data-testid="rating-input-label">
+          Avaliação
+          <input
+            type="number"
+            id="rating-input"
+            value={ rating }
+            data-testid="rating-input"
+            onChange={ (event) => this.setState({ rating: event.target.value }) }
+          />
+        </label>
+        <label htmlFor="genre-input" data-testid="genre-input-label">
+          Gênero
+          <select
+            data-testid="genre-input"
+            value={ genre }
+            id="genre-input"
+            onChange={ (event) => this.setState({ genre: event.target.value }) }
+          >
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
+        </label>
+        <button data-testid="send-button" type="button" onClick={ this.AddMovie }>Adicionar filme</button>
       </form>
     );
   }
