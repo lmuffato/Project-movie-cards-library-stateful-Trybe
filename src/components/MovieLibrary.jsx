97f5bd66f048bff25db10535bafab52659355e54
Nathalia Zebral <natzebral@gmail.com>
2021-05-03 16:32:20 -0300

    alteracao no arquivo MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,30 @@
 // implement AddMovie component here
+import React from 'react';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+//  constructor(props) {
+//    super(props);
+//  }
+
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <h2> awesome movies </h2>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
+export default MovieLibrary;
