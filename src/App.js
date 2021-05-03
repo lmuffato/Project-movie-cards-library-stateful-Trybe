8418b75d5b722f5ccfd47648fb5f2d004626edea
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-05-03 17:46:48 -0300

    test

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar movies={ movies } />
       <MovieList movies={ movies } />
+      <SearchBar />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,30 +1,25 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-
-class SearchBar extends React.Component(this.props) {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        const { movies } = this.props;
-const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } =  movies;
-console.log(this.props)
-        return (
-            <form>
-            </form>
-        )
-    }
+function SearchBar({
+  searchText,
+  onSearchTextChange,
+  bookmarkedOnly,
+  onBookmarkedChange,
+  selectedGenre,
+  onSelectedGenreChange,
+}) {
+    return (
+        <form data-testid="search-bar-form">
+            <label data-testid="text-input-label">
+                Inclui o texto:
+                <input type="text" data-testid="text-input" value={searchText} onChange={onSearchTextChange}>
+                </input>
+            </label>
+        </form>
+    )
 }
 
-SearchBar.propTypes = {
-    SearchBar: PropTypes.shape({
-        searchText: PropTypes.string.isRequired,
-        onSearchTextChange: PropTypes.func.isRequired,
-        bookmarkedOnly: PropTypes.bool.isRequired,
-        onBookmarkedChange: PropTypes.func.isRequired,
-        selectedGenre: PropTypes.string.isRequired,
-        onSelectedGenreChange: PropTypes.func.isRequired
-    }).isRequired
-}
+
+
 export default SearchBar;
