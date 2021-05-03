9cc5b859d728fbe08954736c4bb11729b6c2725c
Mateus Alencar <mtauws@gmail.com>
2021-05-03 18:57:57 -0300

    chore: requirement 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
-class MovieLibrary extends Component {
+/**
+ * Consultei o repositório da malves0 para resolver esse requisito.
+ * link: https://github.com/tryber/sd-09-project-movie-cards-library-stateful/tree/malves0-movie-cards-library-stateful
+ */
+export default class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
-      <div />
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+        />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
     );
   }
 }
 
-export default MovieLibrary;
+MovieLibrary.propTypes = {
+  movies: PropTypes.string,
+}.isRequired;
