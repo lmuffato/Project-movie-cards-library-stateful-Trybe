50d30a2bae3b72314baeb66445ca4310bff5af87
madruga665 <madruga665@hotmail.com>
2021-05-03 16:56:59 -0300

    Requisito 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,12 +12,32 @@ class SearchBar extends React.Component {
     //   // onSelectedGenreChange,
     // } = this.props;
     return (
-      <div className="search-bar">
+      <div>
         <form data-testid="search-bar-form">
-          <label htmlFor="search-input" data-testid="text-input-label">
-            Inclui o texto:
-            <input id="search-input" type="text" value="" />
-          </label>
+          <div className="input-group">
+            <label htmlFor="search-input" data-testid="text-input-label">
+              Inclui o texto:
+              <input
+                id="search-input"
+                type="text"
+                value="searchText"
+                onChange="onSearchTextChange"
+                data-testid="text-input"
+              />
+            </label>
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
+              Mostrar somente favoritos
+              <input
+                id="checkbox-input"
+                type="checbox"
+                onChange="onBookmarkedChange"
+                data-testid="checkbox-input"
+              />
+            </label>
+          </div>
         </form>
       </div>
     );
