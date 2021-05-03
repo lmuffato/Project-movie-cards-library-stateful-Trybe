e59cc15f181ac1f2ff0041da923ff625d4642eb4
Marcelo Maurício <mmauricio.design@gmail.com>
2021-05-03 16:44:27 -0300

    Requisito 5 - Renderiza um select dentro do Form de SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,3 @@
-// implement AddMovie component here
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -11,12 +10,6 @@ class SearchBar extends React.Component {
       selectedGenre,
       onSelectedGenreChange } = this.props;
     console.log(this.props);
-    console.log(searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange);
 
     return (
       <form data-testid="search-bar-form">
@@ -37,6 +30,19 @@ class SearchBar extends React.Component {
             onChange={ onBookmarkedChange }
             data-testid="checkbox-input"
           />
+        </label>
+        <label data-testid="select-input-label" htmlFor="select">
+          Filtrar por gênero
+          <select
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
         </label>
       </form>
     );
