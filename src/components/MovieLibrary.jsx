ead02262ecb55ecfa7bb5d7afa093d4936bcec8e
nascjoao <oi@joaonasc.dev>
2021-05-03 15:53:58 -0300

    ✔ Requisitos 16 e 17: Renderize <SearchBar /> dentro de <MovieLibrary />

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,9 +1,45 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 export default class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
-      <div />
+      <>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={
+            (event) => this.setState({ searchText: event.target.value })
+          }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={
+            (event) => this.setState({ bookmarkedOnly: event.target.value })
+          }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={
+            (event) => this.setState({ selectedGenre: event.target.value })
+          }
+        />
+        <MovieList movies={ movies } />
+      </>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+}.isRequired;
