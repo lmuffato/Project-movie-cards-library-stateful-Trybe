36c69df4be0fecafc7d3bf61aaecbc0fd1ef417f
Samuel Melo <samuelmmjunior@gmail.com>
2021-05-03 20:28:09 -0300

    requisito 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,15 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function SearchBar({ 
+  searchText,
+  onSearchTextChange,
+  bookmarkedOnly,
+  onBookmarkedChange,
+  selectedGenre,
+  onSelectedGenreChange,props 
+}) {
+  
+}
+
+export default SearchBar;
