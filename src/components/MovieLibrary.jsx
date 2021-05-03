055844e88a6f07aa93450f3ff43987fc982e3dec
Luiz Felipe Araujo <thelastnailmusic@gmail.com>
2021-05-03 17:54:42 -0300

    1 a 7 OK - 8 em andamento

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,7 +1,7 @@
 // implement AddMovie component here
 
 import React, { Component } from 'react';
-
+import propTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
@@ -12,15 +12,26 @@ class MovieLibrary extends Component {
   // }
 
   render() {
+    const { movies } = this.props;
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={ this.props.movies } />
+        <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: propTypes.shape({
+    title: propTypes.string,
+    subtitle: propTypes.string,
+    storyline: propTypes.string,
+    rating: propTypes.oneOfType([propTypes.string, propTypes.number]),
+    imagePath: propTypes.string,
+  }).isRequired,
+};
 
 export default MovieLibrary;
