bd8ea7a87dfb6db72fef89cf2691ddbe42748fb1
Marilia Cegalla Aldrighi <maricegalla@gmail.com>
2021-05-03 20:14:28 -0300

    Requisito 8

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
+      <AddMovie />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,11 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      title: event.target.value,
+    });
+  }
+
   render() {
     const { onClick } = this.props;
+    const { title } = this.state;
     return (
       <div value={ onClick }>
-        <form data-testid="add-movie-form" />
+        <form data-testid="add-movie-form">
+          <label htmlFor="text-input" data-testid="title-input-label">
+            Título
+            <input
+              type="text"
+              data-testid="title-input"
+              id="text-input"
+              onChange={ this.handleChange }
+              value={ title }
+            />
+          </label>
+        </form>
       </div>
     );
   }
