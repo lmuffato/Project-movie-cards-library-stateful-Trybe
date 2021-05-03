3318db34c699dde811252809a58a8dba02414f2b
Luiz Felipe Araujo <thelastnailmusic@gmail.com>
2021-05-03 17:29:39 -0300

    1 a 5 OK

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
-import './App.css';
-import MovieLibrary from './components/MovieLibrary';
+// import './App.css';
+// import MovieLibrary from './components/MovieLibrary';
 import SearchBar from './components/SearchBar';
 
 function App() {
