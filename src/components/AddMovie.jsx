f4654d4c63079e0a1acbce340b94b91a9ee97af0
madruga665 <madruga665@hotmail.com>
2021-05-03 18:23:18 -0300

    Add componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,13 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+      <form>
+        <h1>aqui vai o forms pra add novo filme :) </h1>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
