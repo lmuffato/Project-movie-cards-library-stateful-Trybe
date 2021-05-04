51efe1e194360a8d0a3f42338796f02faabcbcd4
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-05-04 09:30:29 -0300

    :rocket: Requisitos 1 e 2

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
+// import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
+
+    const { movies } = this.props;
+
+    this.state = {
+      movies,
+    };
   }
 
   render() {
+    const { movies } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={ this.props.movies } />
-        <AddMovie />
+        <MovieList movies={ movies } />
+        {/* <AddMovie /> */}
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,38 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './styles/SearchBar.css';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+    return (
+      <div className="searchBar">
+        <form data-testid="search-bar-form" />
+      </div>
+    );
+  }
+}
+
+SearchBar.propTypes = PropTypes.shape({
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+});
+
+export default SearchBar;
