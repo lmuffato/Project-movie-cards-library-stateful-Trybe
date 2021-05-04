863d72b767642d5d69f7f3305193bd9011807593
andy silva <andssilva.23@gmail.com>
2021-05-04 16:23:35 -0400

    fix (requirement 6): :bug: Cria o estado inicial do <AddMovie />, Cria o PropTypes do <AddMovie />

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,13 +1,33 @@
 // implement AddMovie component here
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const INITIAL_STATE = {
+  subtitle: '',
+  title: '',
+  imagePath: '',
+  storyline: '',
+  rating: 0,
+  genre: 'action',
+};
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = INITIAL_STATE;
+  }
+
   render() {
+    const { onClick } = this.props;
     return (
       <>
       </>
     );
   }
 }
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default AddMovie;
