8dc930413c26bdfceaf8e4b66c805844b8d9cb55
igson <igson660@gmail.com>
2021-05-04 17:00:41 -0500

    quesitos 06 ao 09

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,56 @@
 // implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  handleTitle = (event) => {
+    this.setState({ title: event.target.value });
+  }
+
+  handlesub = (event) => {
+    this.setState({ subtitle: event.target.value });
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyLine, rating, genre } = this.state;
+
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title-input">
+          Título
+          <input
+            type="text"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleTitle }
+          />
+        </label>
+        <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
+          Subtítulo
+          <input
+            type="text"
+            value={ subtitle }
+            data-testid="subtitle-input"
+            onChange={ this.handlesub }
+          />
+        </label>
+
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
+
--- src/tests/AddMovie.test.js
+++ src/tests/AddMovie.test.js
@@ -54,32 +54,32 @@ beforeEach(() => {
 });
 
 describe('6 - Crie um componente chamado `<AddMovie />`', () => {
-  it.skip('Renderize o componente', () => {
+  it('Renderize o componente', () => {
     render(<AddMovie onClick={ () => jest.fn() } />);
   });
 });
 
 describe('7 - Renderize um formulário dentro de `<AddMovie />`', () => {
-  it.skip('Renderize 1, e apenas 1, form', () => {
+  it('Renderize 1, e apenas 1, form', () => {
     expect(form).toHaveLength(1);
   });
 });
 
 describe('8 - Renderize um input do tipo texto dentro do formulário em `<AddMovie />` para obter o título do novo filme', () => {
-  it.skip('Renderize um input de texto para quem usa escrever o titulo do filme', () => {
+  it('Renderize um input de texto para quem usa escrever o titulo do filme', () => {
     expect(titleInput).toBeInTheDocument();
   });
 
-  it.skip('Renderize a label "Título" para o input de titulo', () => {
+  it('Renderize a label "Título" para o input de titulo', () => {
     expect(titleInputLabel).toBeInTheDocument();
     expect(titleInputLabel).toHaveTextContent('Título');
   });
 
-  it.skip('Defina o estado inicial do titulo como "", ou seja, uma string vazia', () => {
+  it('Defina o estado inicial do titulo como "", ou seja, uma string vazia', () => {
     expect(titleInput).toHaveValue(initialState.title);
   });
 
-  it.skip('Altere o valor do input de título quando algo for digitado nele', () => {
+  it('Altere o valor do input de título quando algo for digitado nele', () => {
     event.type(titleInput, 'my awesome movie title');
 
     expect(titleInput).toHaveValue('my awesome movie title');
@@ -87,20 +87,20 @@ describe('8 - Renderize um input do tipo texto dentro do formulário em `<AddMov
 });
 
 describe('9 - Renderize um input do tipo texto dentro do formulário em `<AddMovie />` para obter o subtítulo do novo filme', () => {
-  it.skip('Renderize um input de texto para quem usa escrever o subtítulo do filme', () => {
+  it('Renderize um input de texto para quem usa escrever o subtítulo do filme', () => {
     expect(subtitleInput).toBeInTheDocument();
   });
 
-  it.skip('Renderize a label "Subtítulo" para o input de subtitulo', () => {
+  it('Renderize a label "Subtítulo" para o input de subtitulo', () => {
     expect(subtitleInputLabel).toBeInTheDocument();
     expect(subtitleInputLabel).toHaveTextContent('Subtítulo');
   });
 
-  it.skip('Defina o  estado inicial do subtitulo como "", ou seja, uma string vazia', () => {
+  it('Defina o  estado inicial do subtitulo como "", ou seja, uma string vazia', () => {
     expect(subtitleInput).toHaveValue(initialState.subtitle);
   });
 
-  it.skip('Altere o valor do input de subtitulo quando algo é digitado nele', () => {
+  it('Altere o valor do input de subtitulo quando algo é digitado nele', () => {
     event.type(subtitleInput, 'my awesome movie subtitle');
 
     expect(subtitleInput).toHaveValue('my awesome movie subtitle');
