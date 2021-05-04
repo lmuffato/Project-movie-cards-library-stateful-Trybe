0fd25fef3303bc271dab14ecec55c3305d85edf9
johnpligeiro <johnpligeiro@gmail.com>
2021-05-04 16:58:20 -0300

    SearchBar fix

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,53 +3,50 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange } = this.props;
 
     return (
-      <div>
-        <form data-testid="search-bar-form" action="">
-          <label htmlFor="text-input" data-testid="text-input-label">
-            Inclui o texto
-            <input
-              onChange={ onSearchTextChange }
-              id="text-input"
-              type="text"
-              data-testid="text-input"
-              value={ searchText }
-            />
-          </label>
-          <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
-            Mostrar somente favoritos
-            <input
-              type="checkbox"
-              data-testid="checkbox-input"
-              id="checkbox-input"
-              checked={ bookmarkedOnly }
-              onChange={ onBookmarkedChange }
-            />
-          </label>
-          <label htmlFor="selectInput" data-testid="select-input-label">
-            Filtrar por gênero
-            <select
-              name="selectInput"
-              id="selectInput"
-              data-testid="select-input"
-              onChange={ onSelectedGenreChange }
-              value={ selectedGenre }
-            >
-              <option value="" data-testid="select-option">Todos</option>
-              <option value="action" data-testid="select-option">Ação</option>
-              <option value="comedy" data-testid="select-option">Comédia</option>
-              <option value="thriller" data-testid="select-option">Suspense</option>
-            </select>
-          </label>
-        </form>
-      </div>
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input-label" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            type="text"
+            data-testid="text-input"
+            onChange={ onSearchTextChange }
+            value={ searchText }
+            id="searchText"
+          />
+        </label>
+
+        <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+            id="bookmarkedOnly"
+          />
+        </label>
+
+        <label htmlFor="select-input" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            name="genre"
+            id="selectedGenre"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
+
+      </form>
     );
   }
 }
