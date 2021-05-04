38786232e30d07c0db06dfe1e00e56a730e9246f
ThalitaC <thalitaceci@gmail.com>
2021-05-04 18:45:33 -0300

    requisito 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,39 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const { filters } = this.props;
-    const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
-    } = filters;
     return (
-      {
-        searchText,
-        onSearchTextChange,
-        bookmarkedOnly,
-        onBookmarkedChange,
-        selectedGenre,
-        onSelectedGenreChange,
-      }
+      <div> teste </div>
     );
   }
 }
-
-SearchBar.propTypes = {
-  filters: PropTypes.shape({
-    searchText: PropTypes.string,
-    onSearchTextChange: PropTypes.func,
-    bookmarkedOnly: PropTypes.bool,
-    onBookmarkedChange: PropTypes.func,
-    selectedGenre: PropTypes.string,
-    onSelectedGenreChange: PropTypes.func,
-  }).isRequired,
-};
 
 export default SearchBar;
