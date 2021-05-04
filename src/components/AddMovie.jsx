0c1c8dc28391c4f08afc9735d8df5d8e03890d51
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-05-04 17:55:25 -0300

    Requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,28 +2,48 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+const INITIAL_STATE = {
+  subtitle: '',
+  title: '',
+  imagePath: '',
+  storyline: '',
+  rating: 0,
+  genre: 'action',
+};
+
 class AddMovie extends Component {
   constructor() {
     super();
-    this.state = {
-      subtitle: '',
-      title: ''
-      imagePath: '',
-      storyline: '',
-      rating: 0,
-      genre: 'action'
-    }
+    this.state = INITIAL_STATE;
   }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
   render() {
     const { onClick } = this.props;
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
-
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="add-movie">
+          <input 
+            id="add-movie"
+            name="title"
+            value={ this.state.title }
+            data-testid="title-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
     );
   }
 }
 
 AddMovie.propTypes = {
-  onClick: PropTypes.func.isRequired
-}
+  onClick: PropTypes.func.isRequired,
+};
 
 export default AddMovie;
