af2b1800d050b90971e74da57d7ebf21504c0a03
Sergio Martins <sergiomos2010@gmail.com>
2021-05-04 16:46:04 -0300

    Requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class MovieLibrary extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      // movies: this.props.movies,
+    };
+  }
+
   render() {
-    const { movies } = this.props;
     return (
-      <>
-      </>
+      <h1>qualquer coisa</h1>
     );
   }
 }
 
-MovieLibrary.propTypes = {
-  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
-};
+// MovieLibrary.propTypes = {
+//   movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+// };
 
 export default MovieLibrary;
