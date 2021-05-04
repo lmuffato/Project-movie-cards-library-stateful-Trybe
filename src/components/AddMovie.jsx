b875957a9262d3429652614bbc276a99c13f0c0c
hheloisa <hhackenhaar@gmail.com>
2021-05-04 15:02:34 -0300

    Constroi base do requisito 12 e 13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -19,10 +19,7 @@ class AddMovie extends React.Component {
       <div>
         <form data-testid="add-movie-form">
 
-          <label
-            data-testid="title-input-label"
-            htmlFor="titleMovie"
-          >
+          <label data-testid="title-input-label" htmlFor="titleMovie">
             Título
             <input
               data-testid="title-input"
@@ -32,10 +29,7 @@ class AddMovie extends React.Component {
             />
           </label>
 
-          <label
-            data-testid="subtitle-input-label"
-            htmlFor="subtitleMovie"
-          >
+          <label data-testid="subtitle-input-label" htmlFor="subtitleMovie">
             Subtítulo
             <input
               data-testid="subtitle-input"
@@ -45,10 +39,7 @@ class AddMovie extends React.Component {
             />
           </label>
 
-          <label
-            data-testid="image-input-label"
-            htmlFor="imageMovie"
-          >
+          <label data-testid="image-input-label" htmlFor="imageMovie">
             Imagem
             <input
               data-testid="image-input"
@@ -58,18 +49,37 @@ class AddMovie extends React.Component {
             />
           </label>
 
-          <label
-            data-testid="storyline-input-label"
-            htmlFor="storylineMovie"
-          >
+          <label data-testid="storyline-input-label" htmlFor="storylineMovie">
             Sinopse
             <textarea
               data-testid="storyline-input"
               id="storylineMovie"
               value={ storyline }
+              type="text"
             />
           </label>
 
+          <label data-testid="rating-input-label" htmlFor="ratingMovie">
+            Avaliação
+            <input
+              data-testid="rating-input"
+              id="ratingMovie"
+              value={ rating }
+              type="text"
+            />
+          </label>
+
+          <label data-testid="genre-input-label" htmlFor="genderMovie">
+            Gênero
+            <select
+              data-testid="genre-input"
+              id="genderMovie"
+            >
+              <option value="action" data-testid="select-option">Ação</option>
+              <option value="comedy" data-testid="select-option">Comédia</option>
+              <option value="thriller" data-testid="select-option">Suspense</option>
+            </select>
+          </label>
         </form>
         <button type="button"> adicionar Filme </button>
       </div>
