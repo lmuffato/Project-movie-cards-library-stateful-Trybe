142cd00aae2b1831078d4fa41cccce89d87aeb03
johnpligeiro <johnpligeiro@gmail.com>
2021-05-04 15:16:08 -0300

    Gender input

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -33,6 +33,18 @@ class AddMovie extends React.Component {
               data-testid="rating-input"
             />
           </label>
+          <label htmlFor="genre-input" data-testid="genre-input-label">
+            Gênero
+            <select
+              name="genre-input"
+              id="genre-input"
+              data-testid="genre-input"
+            >
+              <option value="action" data-testid="genre-option">Ação</option>
+              <option value="comedy" data-testid="genre-option">Comédia</option>
+              <option value="thriller" data-testid="genre-option">Suspense</option>
+            </select>
+          </label>
         </form>
       </div>
     );
