598698fa34c66aa33be2f123bbbbbc47c78c1fb5
AndreBarroso <andre_barroso17@hotmail.com>
2021-05-03 23:13:27 -0300

    Requisitos 6, 7, 8, 9, 10, 11

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -9,21 +9,20 @@ class AddMovie extends React.Component {
       title: '',
       imagePath: '',
       storyline: '',
-      rating: 0,
-      genre: 'action',
     };
   }
 
   changeValue({ target }) {
+    const { name } = target;
     this.setState({
-      title: target.value,
+      [name]: target.value,
     });
   }
 
   render() {
     const {
       subtitle, title, imagePath,
-      storyline, rating, genre } = this.state;
+      storyline } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="title-input" label data-testid="title-input-label">
@@ -32,6 +31,34 @@ class AddMovie extends React.Component {
             onChange={ this.changeValue }
             value={ title }
             data-testid="title-input"
+            name="title"
+          />
+        </label>
+        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            onChange={ this.changeValue }
+            value={ subtitle }
+            data-testid="subtitle-input"
+            name="subtitle"
+          />
+        </label>
+        <label htmlFor="image-input" data-testid="image-input-label">
+          Imagem
+          <input
+            onChange={ this.changeValue }
+            value={ imagePath }
+            data-testid="image-input"
+            name="imagePath"
+          />
+        </label>
+        <label htmlFor="storyline-input" data-testid="storyline-input-label">
+          Sinopse
+          <input
+            onChange={ this.changeValue }
+            value={ storyline }
+            data-testid="storyline-input"
+            name="storyline"
           />
         </label>
       </form>
