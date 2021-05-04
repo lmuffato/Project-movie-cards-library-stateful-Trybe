6b71d0b64ba32c4de1a5e07e9ddf41b687037d9d
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-05-04 18:02:01 -0300

    Requisito 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -28,12 +28,20 @@ class AddMovie extends Component {
     const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label data-testid="title-input-label" htmlFor="add-movie">
-          <input 
-            id="add-movie"
+        <label data-testid="title-input-label" htmlFor="title">
+          <input
             name="title"
-            value={ this.state.title }
+            value={ title }
             data-testid="title-input"
+            onChange={ this.handleChange }
+          />
+        </label >
+        <label data-testid="subtitle-input-label" htmlFor="subtitle">
+          Subtítulo
+          <input
+            name="subtitle"
+            value={ subtitle }
+            data-testid="subtitle-input"
             onChange={ this.handleChange }
           />
         </label>
