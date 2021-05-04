0e7f2412b4ff1e7a8ddb2cf734416de286b93a79
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-05-04 08:43:32 -0300

    Requisito 6, addmovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,27 @@
-// implement AddMovie component here
+import React from 'react';
+
+// this.state = {
+//   subtitle: '',
+//   title: '',
+//   imagePath: '',
+//   storyline: '',
+//   rating: 0,
+//   genre: 'action',
+// };
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+      <form>
+        subtítulo
+        título
+        caminho da imagem
+        sinopse
+        avaliação
+        gênero
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
