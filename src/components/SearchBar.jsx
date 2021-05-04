6d1e3440d1e083cdb92643187c3b2f01c964b2ae
andy silva <andssilva.23@gmail.com>
2021-05-04 14:37:59 -0400

    feat (requirement 3): :rocket: Renderiza um input text dentro do form de <SearchBar />

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,17 @@ class SearchBar extends React.Component {
   render() {
     return (
       <form data-testid="search-bar-form">
-        teste
+        <label htmlFor="search-area" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            id="search-area"
+            name="search-area"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
       </form>
     );
   }
