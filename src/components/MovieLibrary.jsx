5adb3af1b72264add12fd795b9190c98d6b19a27
AdelinoJnr <adelinojunior96@gmail.com>
2021-05-04 10:27:25 -0300

    Requisito 15 Crie um componente chamado MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,20 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieLibrary extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(),
+};
+
+export default MovieLibrary;
