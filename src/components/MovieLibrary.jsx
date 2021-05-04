00beb98add5aa50e0ce9595f558dbf43a97c1c7b
madruga665 <madruga665@hotmail.com>
2021-05-04 16:04:23 -0300

    Requisitos 17 e 18

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,19 @@
-// implement AddMovie component here
+import React from 'react';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import movies from '../data';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
