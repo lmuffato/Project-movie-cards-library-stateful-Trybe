3c8dec3c42da62058bced34e6493a481ce28cc3e
hheloisa <hhackenhaar@gmail.com>
2021-05-04 14:18:25 -0300

    Resolve requisito 8, exceto a funçao

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
   render() {
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <div>
+        <form data-testid="add-movie-form">
+
+          <label
+            data-testid="title-input-label"
+            htmlFor="titleMovie"
+          >
+            Título
+            <input
+              data-testid="title-input"
+              id="titleMovie"
+              type="text"
+              value={ title }
+            />
+          </label>
+        </form>
         <button type="button"> adicionar Filme </button>
       </div>
     );
