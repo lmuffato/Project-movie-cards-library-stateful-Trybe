4a01903af3b94d4a506492ba2e195747fe7d5fb3
Rafael Ribeiro <rafittu@outlook.com>
2021-05-04 14:18:57 -0300

    recupera requisito 12

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,64 +1,64 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-
-// class Rating extends React.Component {
-// render() {
-//   const { rating } = this.props;
-
-//     return (
-//       <div className="movie-card-rating" data-testid="rating">
-//         <span className="rating">{rating}</span>
-//       </div>
-//     );
-//   }
-// }
-
-// Rating.propTypes = { rating: PropTypes.number };
-
-// Rating.defaultProps = {
-//   rating: 'undefined',
-// };
-
-// export default Rating;
-
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Rating extends Component {
+class Rating extends React.Component {
   render() {
-    const { value, handleChange } = this.props;
     const { rating } = this.props;
 
     return (
-      <main>
-        <div className="movie-card-rating" data-testid="rating">
-          <span className="rating">{rating}</span>
-        </div>
-
-        <label data-testid="rating-input-label" htmlFor="input-rating">
-          Avaliação
-          <input
-            type="number"
-            name="rating"
-            value={ value }
-            onChange={ handleChange }
-            data-testid="rating-input"
-          />
-        </label>
-      </main>
+      <div className="movie-card-rating" data-testid="rating">
+        <span className="rating">{rating}</span>
+      </div>
     );
   }
 }
 
-Rating.propTypes = {
-  rating: PropTypes.number,
-  value: PropTypes.number,
-  handleChange: PropTypes.func.isRequired,
-};
+Rating.propTypes = { rating: PropTypes.number };
 
 Rating.defaultProps = {
-  value: 'undefined',
   rating: 'undefined',
 };
+
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+// class Rating extends Component {
+//   render() {
+//     const { value, handleChange } = this.props;
+//     const { rating } = this.props;
+
+//     return (
+//       <main>
+//         <div className="movie-card-rating" data-testid="rating">
+//           <span className="rating">{rating}</span>
+//         </div>
+
+//         <label data-testid="rating-input-label" htmlFor="input-rating">
+//           Avaliação
+//           <input
+//             type="number"
+//             name="rating"
+//             value={ value }
+//             onChange={ handleChange }
+//             data-testid="rating-input"
+//           />
+//         </label>
+//       </main>
+//     );
+//   }
+// }
+
+// Rating.propTypes = {
+//   rating: PropTypes.number,
+//   value: PropTypes.number,
+//   handleChange: PropTypes.func.isRequired,
+// };
+
+// Rating.defaultProps = {
+//   value: 'undefined',
+//   rating: 'undefined',
+// };
+
+// export default Rating;
 
 export default Rating;
