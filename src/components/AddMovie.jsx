b208d655d561047315c8d9cbb1aebd6468ebd87b
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-05-04 16:41:41 -0300

    Requisito 11

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -21,45 +21,74 @@ class AddMovie extends React.Component {
     this.setState(({ [name]: value }));
   };
 
+  inputTitle = (title) => (
+    <label htmlFor="campo1" data-testid="title-input-label">
+      Título
+      <input
+        name="title"
+        id="campo1"
+        data-testid="title-input"
+        type="text"
+        value={ title }
+        onChange={ this.handleChange }
+      />
+    </label>
+  )
+
+  inputSubtitle = (subtitle) => (
+    <label htmlFor="campo2" data-testid="subtitle-input-label">
+      Subtítulo
+      <input
+        name="subtitle"
+        id="campo2"
+        data-testid="subtitle-input"
+        type="text"
+        value={ subtitle }
+        onChange={ this.handleChange }
+      />
+    </label>
+  )
+
+  inputImage = (imagePath) => (
+    <label htmlFor="campo3" data-testid="image-input-label">
+      Imagem
+      <input
+        name="imagePath"
+        id="campo3"
+        data-testid="image-input"
+        type="text"
+        value={ imagePath }
+        onChange={ this.handleChange }
+      />
+    </label>
+  );
+
+  inputStory = (storyline) => (
+    <label htmlFor="campo4" data-testid="storyline-input-label">
+      Sinopse
+      <textarea
+        name="storyline"
+        id="campo4"
+        data-testid="storyline-input"
+        type="text"
+        value={ storyline }
+        onChange={ this.handleChange }
+      />
+    </label>
+  );
+  // inputRating = (rating) => ();
+  // inputGenre = (genre) => ();
+
   render() {
     const { onClick } = this.props;
     const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <div>
         <form data-testid="add-movie-form">
-          <label htmlFor="campo1" data-testid="title-input-label">
-            Título
-            <input
-              name="title"
-              id="campo1"
-              data-testid="title-input"
-              type="text"
-              value={ title }
-              onChange={ this.handleChange }
-            />
-          </label>
-          <label htmlFor="campo2" data-testid="subtitle-input-label">
-            Subtítulo
-            <input
-              name="subtitle"
-              id="campo2"
-              data-testid="subtitle-input"
-              type="text"
-              value={ subtitle }
-              onChange={ this.handleChange }
-            />
-          </label>
-          <label htmlFor="campo3" data-testid="image-input-label">
-            Imagem
-            <input
-              name="imagePath"
-              id="campo3"
-              data-testid="image-input"
-              type="text"
-              value={ imagePath }
-              onChange={ this.handleChange }
-            />
-          </label>
+          {this.inputTitle(title)}
+          {this.inputSubtitle(subtitle)}
+          {this.inputImage(imagePath)}
+          {this.inputStory(storyline)}
         </form>
       </div>
     );
