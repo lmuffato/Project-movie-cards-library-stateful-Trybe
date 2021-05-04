091e9cccec2f5e43452c90f1c921ca161ef4c143
Gustavo Sousa <gustavohl98@gmail.com>
2021-05-04 16:00:51 -0300

    Requisito 06 - cria componente AddMovie

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import MovieList from './components/MovieList';
+import AddMovie from './components/AddMovie';
 import movies from './data';
 import './App.css';
 
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <SearchBar />
       <MovieList movies={ movies } />
+      <AddMovie />
     </div>
   );
 }
