03fe6e13707d2238a9706608f7ab135bf6c8ec6b
Guilherme de Prais <guilherme.prais@gmail.com>
2021-05-04 17:52:31 -0300

    requirement 06

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,23 @@
-// implement AddMovie component here
+import React from 'react';
+
+class App extends React.Component {
+  constructor({ onClick }) {
+    super(onClick);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default App;
