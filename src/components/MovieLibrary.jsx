5437cdd7b7779001187b7e8f69774353c7e21ef9
Willian Mendes <willianmendesbr@gmail.com>
2021-05-04 11:59:57 -0300

    component movielibrary criado - Requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieCard from './MovieCard';
 
 class MovieLibrary extends Component {
-  render() {
+  constructor(props) {
+    super(props);
     const { movies } = this.props;
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
       <div>
-        {movies.map((movie) => `${movie.title} - ${movie.subtitle}`)}
+        <SearchBar
+          searchText={ searchText }
+          // onSearchTextChange={}
+          bookmarkedOnly={ bookmarkedOnly }
+          // onBookmarkedChange={}
+          selectedGenre={ selectedGenre }
+          // onSelectedGenreChange={}
+        />
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }
