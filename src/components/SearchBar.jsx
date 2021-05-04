5886498047d9fd55cade0cda89c890d127004f87
johnpligeiro <johnpligeiro@gmail.com>
2021-05-04 13:06:25 -0300

    prop search text

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
+    const { searchText } = this.props;
+
     return (
       <div>
         <form data-testid="search-bar-form" action="">
           <label htmlFor="text-input" data-testid="text-input-label">
             Inclui o texto:
-            <input id="text-input" type="text" data-testid="text-input" />
+            <input
+              id="text-input"
+              type="text"
+              data-testid="text-input"
+              value={ searchText }
+            />
           </label>
           <input type="checkbox" />
         </form>
