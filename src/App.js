23460a7123e16905a64d25b5f847c6400cf87b79
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-05-04 08:35:32 -0300

    Iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import movies from './data';
 import Header from './components/Header';
 import './App.css';
+import MovieLibrary from './components/MovieLibrary';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <MovieLibrary movies={ movies } />
+      </div>
+    );
+  }
 }
 
 export default App;
