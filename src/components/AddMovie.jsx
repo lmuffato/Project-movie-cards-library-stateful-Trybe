bf97cbb7eb6097c99436943f9c5259b600ea1664
Willian Mendes <willianmendesbr@gmail.com>
2021-05-03 21:41:10 -0300

    implements function to create inputs dynamic

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import event from '@testing-library/user-event';
+// import PropTypes from 'prop-types';
 
 class AddMovie extends Component {
   constructor(props) {
@@ -11,7 +10,7 @@ class AddMovie extends Component {
       imagePath: '',
       storyline: '',
       rating: 0,
-      genre: 'action',
+      // genre: 'action',
     };
 
     this.changeStateValue = this.changeStateValue.bind(this);
@@ -23,67 +22,54 @@ class AddMovie extends Component {
     });
   }
 
+  createInput(type, state, stateObj, name) {
+    let stateValue = stateObj[state];
+    let CustomTag = '';
+    const tagImgLabel = 'image-input-label';
+    const tagImg = 'image-input';
+
+    if (stateValue === null) stateValue = '';
+
+    if (type === 'textarea') {
+      CustomTag = 'textarea';
+    } else {
+      CustomTag = 'input';
+    }
+
+    return (
+      <label
+        htmlFor={ state }
+        data-testid={ state !== 'imagePath' ? `${state}-input-label` : tagImgLabel }
+      >
+        {name}
+        <CustomTag
+          type={ type }
+          id={ state }
+          value={ stateValue }
+          onChange={ (e) => this.changeStateValue(e, state) }
+          data-testid={ state !== 'imagePath' ? `${state}-input` : tagImg }
+        />
+      </label>
+    );
+  }
+
   render() {
-    const { onClick } = this.props;
+    // const { onClick } = this.props;
     const { title, subtitle, imagePath, storyline, rating } = this.state;
     return (
       <form action="#" data-testid="add-movie-form">
-        <label htmlFor="title" data-testid="title-input-label">
-          Título
-          <input
-            type="text"
-            id="title"
-            value={ title }
-            onChange={ (e) => this.changeStateValue(e, 'title') }
-            data-testid="title-input"
-          />
-        </label>
-        <label htmlFor="subtitle" data-testid="subtitle-input-label">
-          Subtítulo
-          <input
-            type="text"
-            id="subtitle"
-            value={ subtitle }
-            onChange={ (e) => this.changeStateValue(e, 'subtitle') }
-            data-testid="subtitle-input"
-          />
-        </label>
-        <label htmlFor="image" data-testid="image-input-label">
-          Imagem
-          <input
-            type="text"
-            id="image"
-            value={ imagePath }
-            onChange={ (e) => this.changeStateValue(e, 'imagePath') }
-            data-testid="image-input"
-          />
-        </label>
-        <label htmlFor="storyline" data-testid="storyline-input-label">
-          Sinopse
-          <textarea
-            id="storyline"
-            value={ storyline }
-            onChange={ (e) => this.changeStateValue(e, 'storyline') }
-            data-testid="storyline-input"
-          />
-        </label>
-        <label htmlFor="rating" data-testid="rating-input-label">
-          Avaliação
-          <input
-            type="number"
-            id="rating"
-            value={ rating }
-            onChange={ (e) => this.changeStateValue(e, 'rating') }
-            data-testid="rating-input"
-          />
-        </label>
+        {this.createInput('text', 'title', { title }, 'Título')}
+        {this.createInput('text', 'subtitle', { subtitle }, 'Subtítulo')}
+        {this.createInput('text', 'imagePath', { imagePath }, 'Imagem')}
+        {this.createInput('textarea', 'storyline', { storyline }, 'Sinopse')}
+        {this.createInput('number', 'rating', { rating }, 'Avaliação')}
       </form>
     );
   }
 }
 
-AddMovie.propTypes = {
-  onClick: PropTypes.func.isRequired,
-};
+// AddMovie.propTypes = {
+//   onClick: PropTypes.func.isRequired,
+// };
 
 export default AddMovie;
