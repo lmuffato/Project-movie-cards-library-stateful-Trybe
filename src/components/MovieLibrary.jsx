aa5c71f6842df74829a7f66e4ae494fc58b0bb48
Maria Baeta <m.martinsdg@gmail.com>
2021-05-04 16:47:50 -0300

    Requisito 16 - 14 ainda nao passou

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -32,7 +32,7 @@ class MovieLibrary extends React.Component {
   }
 
   handleOnClick(event) {
-    console.log(event.target.value);
+    console.log(event.target);
     event.preventDefault();
   }
 
