8f339c516786a6bfaa17658441e0bb062726ab2b
Eduardo Seije <eduardoseije@gmail.com>
2021-05-04 11:46:19 -0300

    Requisito 15 - teste 01

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,57 @@
 // implement AddMovie component here.
+import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import AddMovies from './AddMovie';
+import MovieList from './MovieList';
+
+class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      // movies,
+    };
+  }
+
+  handleSearchChange = ({ target }) => {
+    const { value } = target;
+    this.setState = {
+      searchText: value,
+    };
+  }
+
+  handleGenderChange = ({ target }) => {
+    const { value } = target;
+    this.setState = {
+      selectedGenre: value,
+    };
+  }
+
+  render() {
+    const { movies } = this.props;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    return (
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.handleSearchChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.handleGenderChange }
+          movies={ movies }
+        />
+        <MovieList movies={ movies } />
+        <AddMovies />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf.isRequired,
+};
+
+export default MovieLibrary;
