89f7a42ceddb827adb1ee99cb5a723fb36cdc771
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-05-04 16:50:06 -0300

    first commit

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,55 @@
-// implement AddMovie component here
+import React from 'react';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import movies from '../data'
+
+class MovieLibrary extends React.Component {
+  constructor () {
+    super();
+    
+    this.onBookmarkedChanged = this.onBookmarkedChanged.bind(this)
+    this.onSearchTextChange = this.onSearchTextChange.bind(this)
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this)
+
+    this.state ={
+      searchText: '',
+      selectedGenre: '',
+      bookmarkedOnly: false,
+    }
+  }
+
+  onSearchTextChange() {
+
+  }
+
+  onBookmarkedChanged() {
+
+  }
+
+  onSelectedGenreChange() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <form>
+        <SearchBar 
+        searchText={this.state.searchText}
+        onSearchTextChange={this.onSearchTextChange}
+        onBookmarkedChanged={this.onBookmarkedChanged}
+        selectedGenre={this.state.selectedGenre}
+        onSelectedGenreChange={this.onSelectedGenreChange}
+        bookmarkedOnly={this.state.bookmarkedOnly}
+        />
+        <MovieList movies={movies} />
+        <AddMovie />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,37 @@
-// implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render(){
+    const { searchText, onSearchTextChange, onBookmarkedChanged } = this.props
+
+    return(
+      <div>
+      <label data-testid="text-input-label">
+        Inclui o texto: 
+      <input 
+      type="text"
+      name="searchedText"
+      value={searchText}
+      onChange={onSearchTextChange}
+      data-testid="text-input"
+      >
+      </input>
+      </label>
+      <br />
+      <label data-testid="checkbox-input-label">
+      Mostrar somente favoritos
+      <input 
+      type="checkbox"
+      data-testid="checkbox-input"
+      name="bookmarkedOnly"
+      onChange={onBookmarkedChanged}
+      >
+      </input>
+      </label>
+
+      </div>
+    )
+  }
+}
+
+export default SearchBar;
