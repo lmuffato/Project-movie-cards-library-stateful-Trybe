a9c3ae72fc6d852ee094eeb950f7c038512bc170
Gustavo Sousa <gustavohl98@gmail.com>
2021-05-04 16:08:56 -0300

    Requisito 07 - renderiza um form dentro de AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  /*
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+  */
   render() {
     return (
-      <div />
+      <div>
+        <form data-testid="add-movie-form" />
+      </div>
     );
   }
 }
