5e2f828b0757598f0e52347b2522f6cd63293543
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-05-04 19:03:53 -0300

    Requisitos 5+ :heavy-check-mark:

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Header from './components/Header';
 import './App.css';
 import MovieList from './components/MovieList';
+import AddMovie from './components/AddMovie';
 import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList movies={ movies } />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,64 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            value={ title }
+            data-testid="title-input"
+            onChange={ (event) => this.setState({ title: event.target.value }) }
+          />
+        </label>
+        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            value={ subtitle }
+            data-testid="subtitle-input"
+            onChange={ (event) => this.setState({ subtitle: event.target.value }) }
+          />
+        </label>
+        <label htmlFor="image-input" data-testid="image-input-label">
+          Imagem
+          <input
+            type="text"
+            value={ imagePath }
+            data-testid="image-input"
+            onChange={ (event) => this.setState({ imagePath: event.target.value }) }
+          />
+        </label>
+        <label htmlFor="storyline-input" data-testid="storyline-input-label">
+          Sinopse
+          <input
+            type="textarea"
+            value={ storyline }
+            data-testid="storyline-input"
+            onChange={ (event) => this.setState({ storyline: event.target.value }) }
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
