0ba780234a5120851ee711eeb46efeb88276b702
Orlando Flores <orlando_flores@id.uff.br>
2021-05-03 23:22:05 -0300

    requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,12 +2,26 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
   render() {
+    const { movies } = this.props;
     return (
       <div>
         <SearchBar />
+        <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
