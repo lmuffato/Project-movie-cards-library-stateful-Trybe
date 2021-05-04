86db3f9e22cdfb098a75b191e6de0835458bd2da
Marcelo Maurício <mmauricio.design@gmail.com>
2021-05-04 16:09:35 -0300

    Requisito 16 e 17 - Configura o state inicial de MovieLibrary e o 17 Renderiza o componente SearchBar dentro de MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
-      <div />
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={
+            (event) => this.setState({ searchText: event.target.value })
+          }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={
+            (event) => this.setState({ bookmarkedOnly: event.target.value })
+          }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={
+            (event) => this.setState({ selectedGenre: event.target.value })
+          }
+        />
+        <MovieList movies={ movies } />
+      </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.object,
+}.isRequired;
 
 export default MovieLibrary;
