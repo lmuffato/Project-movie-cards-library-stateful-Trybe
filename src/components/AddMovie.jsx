41df5e49f500978da47b00e0b70336df2f77b010
Gustavo Sousa <gustavohl98@gmail.com>
2021-05-04 16:33:36 -0300

    Requisito 08 - renderiza input para titulo dentro de form do AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,23 +1,45 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
-  /*
   constructor(props) {
     super(props);
+
+    this.handleTextChange = this.handleTextChange.bind(this);
     this.state = {
-      subtitle: '',
+    //   subtitle: '',
       title: '',
-      imagePath: '',
-      storyline: '',
-      rating: 0,
-      genre: 'action',
+    //   imagePath: '',
+    //   storyline: '',
+    //   rating: 0,
+    //   genre: 'action',
     };
   }
-  */
+
+  handleTextChange(e) {
+    this.setState({ title: e.target.value });
+  }
+
   render() {
+    const { title } = this.state;
     return (
       <div>
-        <form data-testid="add-movie-form" />
+        <form data-testid="add-movie-form">
+          <div>
+            <label
+              htmlFor="title-input"
+              data-testid="title-input-label"
+            >
+              Título
+              <input
+                type="text"
+                value={ title }
+                onChange={ this.handleTextChange }
+                data-testid="title-input"
+                id="title-input"
+              />
+            </label>
+          </div>
+        </form>
       </div>
     );
   }
