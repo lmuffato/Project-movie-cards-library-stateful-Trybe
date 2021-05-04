89f3f0a8e367bdc6bfbc1775342e67ce855c7a62
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-05-04 08:11:51 -0300

    Startig the project

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,14 @@
 // implement AddMovie component here
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <div>
+        Iniciando o projeto!
+      </div>
+    )
+  }
+}
+
+export default MovieLibrary;
