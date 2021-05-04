5b28ac7f1e906a4268598ed4c5ffff9f3cf57874
andy silva <andssilva.23@gmail.com>
2021-05-04 14:04:50 -0400

    feat (requirement 1): :rocket: Cria um componente <SearchBar />

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,13 @@
 // implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+export default SearchBar;
