55dcc313eb50af43a10ac41a48e089e6c67e28d8
ThalitaC <thalitaceci@gmail.com>
2021-05-04 19:26:35 -0300

    Requisitos 2 e 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const { searchText,
+      onSearchTextChange } = this.props;
     return (
-      <div> teste </div>
+      <form data-testid="search-bar-form">
+        <label htmlFor="inclui-texto" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            data-testid="text-input"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+      </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.string.isRequired,
+};
 
 export default SearchBar;
