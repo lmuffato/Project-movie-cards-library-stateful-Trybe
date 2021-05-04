f8a4bd1a8ab33e247accf546bc67ae04c660c708
johnpligeiro <johnpligeiro@gmail.com>
2021-05-04 16:37:45 -0300

    16 - configurados estados iniciais

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 
-// import MovieList from './MovieList';
+import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor({ movies }) {
+    super();
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
 
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        {/* <MovieList movies={this.props.movies} /> */}
+        <MovieList movies={this.props.movies} />
         <AddMovie />
       </div>
     );
