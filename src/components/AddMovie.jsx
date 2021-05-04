8375e67772ab8de61ea38451b850d0c5a7cbcbe2
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-05-04 18:36:23 -0300

    Requisito 13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -63,7 +63,7 @@ class AddMovie extends Component {
             data-testid="storyline-input"
             onChange={ this.handleChange }
           />
-        </label >
+        </label>
         <label data-testid="rating-input-label" htmlFor="rating">
           Avaliação
           <input 
@@ -72,6 +72,19 @@ class AddMovie extends Component {
             data-testid="rating-input"
             onChange={ this.handleChange }
           />
+        </label>
+        <label data-testid="genre-input-label" htmlFor="genre">
+          Gênero
+          <select
+            name="genre"
+            value = { genre }
+            data-testid="genre-input"
+            onChange={ this.handleChange }
+          >
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select> 
         </label>
       </form>
     );
