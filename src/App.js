45410ca8d8031d9571c7048d9d94fef6ea603de1
AdelinoJnr <adelinojunior96@gmail.com>
2021-05-04 13:42:59 -0300

    Requisito 16 Configure o estado inicial do componente MovieLibray

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React from 'react';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
-import SearchBar from './components/SearchBar';
 import AddMovie from './components/AddMovie';
 import data from './data';
 import './App.css';
+import MovieLibrary from './components/MovieLibrary';
 
 function handleClick(event) {
   event.preventDefault();
   console.log(event.target);
 }
 
+const movies = [
+  {
+    title: 'An awesome title',
+    subtitle: 'Movie Subtitle 1',
+    storyline: 'Movie Storyline 1',
+    rating: 4.5,
+    imagePath: 'images/movie_1',
+    bookmarked: true,
+    genre: 'action',
+  },
+  {
+    title: 'Movie Title 2',
+    subtitle: 'An incredible subtitle',
+    storyline: 'Movie Storyline 2',
+    rating: 4.5,
+    imagePath: 'images/movie_2',
+    bookmarked: false,
+    genre: 'comedy',
+  },
+  {
+    title: 'Movie Title 3',
+    subtitle: 'Movie Subtitle 3',
+    storyline: 'An great storyline',
+    rating: 3,
+    imagePath: 'images/movie_3',
+    bookmarked: false,
+    genre: 'thriller',
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar
-        searchText="string"
-        onSearchTextChange={ console.log }
-        bookmarkedOnly={ false }
-        onBookmarkedChange={ console.log }
-        selectedGenre="string"
-        onSelectedGenreChange={ console.log }
-      />
+      <MovieLibrary movies={movies} />
       <MovieList movies={ data } />
       <AddMovie onClick={ handleClick } />
     </div>
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,13 +1,33 @@
 // implement AddMovie component here
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props,
+    }
+  }
+
   render() {
     const { movies } = this.props;
     return (
       <div>
-        
+        <SearchBar
+          searchText=""
+          onSearchTextChange={ console.log }
+          bookmarkedOnly={ false }
+          onBookmarkedChange={ console.log }
+          selectedGenre=""
+          onSelectedGenreChange={ console.log }
+        />
+       {movies.map((movie) => <MovieCard movie={ movie } />)} 
       </div>
     );
   }
