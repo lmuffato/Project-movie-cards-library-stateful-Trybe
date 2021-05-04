f2be3e9f863781e3c96ec1694cd1b72942895625
Orlando Flores <orlando_flores@id.uff.br>
2021-05-03 23:11:33 -0300

    requisito 15

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
-// import movies from './data';
+import movies from './data';
+import MovieLibrary from './components/MovieLibrary';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
-      <AddMovie />
+      <MovieLibrary movies={ movies } />
     </div>
   );
 }
