b1073544d0f104baa23ffe6ba0c886957acc17c6
johnpligeiro <johnpligeiro@gmail.com>
2021-05-04 13:02:07 -0300

    Label

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 class SearchBar extends React.Component {
   render() {
     return (
-      <form data-testid="search-bar-form" action="">
-        <input data-testid="text-input" type="text" />
-        <input type="checkbox" />
-      </form>
-
+      <div>
+        <form data-testid="search-bar-form" action="">
+          <label htmlFor="text-input" data-testid="text-input-label">
+            Inclui o texto:
+            <input id="text-input" type="text" data-testid="text-input" />
+          </label>
+          <input type="checkbox" />
+        </form>
+      </div>
     );
   }
 }
