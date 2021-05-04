7062cb219f5aaf03648dcd4b1b9931843402e757
Sergio Martins <sergiomos2010@gmail.com>
2021-05-04 18:30:53 -0300

    Metade do requisito 18

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes, { shape } from 'prop-types';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
 
     this.handleChange = this.handleChange.bind(this);
+    this.search = this.search.bind(this);
 
     const { movies } = this.props;
     this.state = {
@@ -24,19 +26,45 @@ class MovieLibrary extends Component {
     this.setState({
       [name]: value,
     });
+
+    this.search();
+  }
+
+  search() {
+    const { searchText, genre } = this.state;
+    const { movies } = this.props;
+
+    if (searchText) {
+      this.setState({
+        movies: movies
+          .filter(({ title, subtitle, storyline }) => title.includes(searchText)
+          || subtitle.includes(searchText)
+          || storyline.includes(searchText)),
+      });
+    }
+
+    if (genre) {
+      this.setState({
+        movies: movies.filter((movie) => movie.genre === genre),
+      });
+    }
   }
 
   render() {
-    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
-      <SearchBar
-        searchText={ searchText }
-        onSearchTextChange={ this.handleChange }
-        bookmarkedOnly={ bookmarkedOnly }
-        onBookmarkedChange={ this.handleChange }
-        selectedGenre={ selectedGenre }
-        onSelectedGenreChange={ this.handleChange }
-      />
+      <>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.handleChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.handleChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.handleChange }
+        />
+
+        <MovieList movies={ movies } />
+      </>
     );
   }
 }
@@ -48,6 +76,8 @@ MovieLibrary.propTypes = {
     storyline: PropTypes.string.isRequired,
     rating: PropTypes.number.isRequired,
     imagePath: PropTypes.string.isRequired,
+    bookmarked: PropTypes.bool.isRequired,
+    genre: PropTypes.string,
   })).isRequired,
 };
 
