5d2e62cc15ca0a8fee94dfd83f95f2f993f3375c
Robertson <robertson.nunes@gmail.com>
2021-05-05 20:44:18 -0300

    Requisito 16

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
+import movies from './data';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
+      <MovieLibrary movies={ movies } />
       <AddMovie onClick="callback" />
     </div>
   );
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,12 +1,38 @@
 // implement AddMovie component here
 import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+  }
+
   render() {
+    const { movies } = this.props;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
-      <div />
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+          movies={ movies }
+        />
+        <MovieList movies={ movies } />
+      </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf.isRequired,
+};
 
 export default MovieLibrary;
