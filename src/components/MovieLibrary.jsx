c8579c9c4f9d57c6bdc7ca4e3216cfa714f4a238
andy silva <andssilva.23@gmail.com>
2021-05-04 23:36:11 -0400

    refactor: :art: Refatorando movies em <MovieLibrary />

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -15,7 +15,7 @@ class MovieLibrary extends Component {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies
+      movies,
     };
   }
 
@@ -39,8 +39,7 @@ class MovieLibrary extends Component {
   }
 
   render() {
-    const { movies } = this.props;
-    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
