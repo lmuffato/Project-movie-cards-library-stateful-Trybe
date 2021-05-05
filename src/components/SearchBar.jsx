4e62c0ff84272a5cc0ea88db77bdf80a370f813e
André Jaques Batista <andrejaques.b@gmail.com>
2021-05-05 08:24:12 -0300

    add requires 01, 02 and 03

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,25 @@
-// implement AddMovie component here
+import React from "react";
+
+export default function SearchBar(props) {
+  const {
+    searchText,
+    onSearchTextChange,
+    bookmarkedOnly,
+    onBookmarkedChange,
+    selectedGenre,
+    onSelectedGenreChange,
+  } = props;
+
+  return (
+    <form className='form-container' data-testid='search-bar-form'>
+      <label className='label-container'>
+        <input
+          type='text'
+          value={searchText}
+          onChange={onSearchTextChange}
+          data-testid='text-input'
+        />
+      </label>
+    </form>
+  );
+}
