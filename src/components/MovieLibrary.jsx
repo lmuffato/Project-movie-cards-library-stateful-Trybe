e863f3ee37022ddde303f6cba23c50607ceab946
andy silva <andssilva.23@gmail.com>
2021-05-04 22:49:54 -0400

    feat (requirement 16 e 17): :rocket: Configura o estado de <MovieLibrary />, Renderiza <SearchBar /> dentro de <MovieLibrary />

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -6,17 +6,52 @@ import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  /* constructor(props) {
+  constructor(props) {
     super(props);
 
-  } */
+    const { movies } = this.props;
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies
+    };
+  }
+
+  textChange = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      searchText: event.target.value,
+    });
+  }
+
+  markedChange = (event) => {
+    this.setState({
+      bookmarkedOnly: event.target.checked,
+    });
+  }
+
+  genreChange = (event) => {
+    this.setState({
+      selectedGenre: event.target.value,
+    });
+  }
 
   render() {
     const { movies } = this.props;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.textChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.markedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.genreChange }
+        />
         <MovieList movies={ movies } />
         <AddMovie />
       </div>
