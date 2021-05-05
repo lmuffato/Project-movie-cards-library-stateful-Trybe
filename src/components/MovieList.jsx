8c106e34564493df2d6d55a40b39881c507c00a8
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-05-05 04:51:26 -0300

    feat(requirements 01,02,03,04 e 05): :rocket: Component SearchBar

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import MovieCard from './MovieCard';
+import SearchBar from './SearchBar';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-
     return (
       <div data-testid="movie-list" className="movie-list">
         { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+        <div>
+          <SearchBar />
+        </div>
       </div>
     );
   }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,50 @@
 // implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly } = this.props;
+    const { onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="input-text">
+          Inclui o texto
+          <input
+            type="text"
+            id="input-text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+        <label htmlFor="input-checkbox" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            id="input-checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
+        <label htmlFor="select" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            id="select"
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
+
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
