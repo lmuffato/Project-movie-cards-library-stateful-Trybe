6745f6ddef8573f5a9a46566f316504933e49e27
Walvesb65 <willian.alvesb65@gmail.com>
2021-05-05 01:20:18 -0300

    Requisitos

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+/* import PropTypes from 'prop-types'; */
 import AddMovie2 from './AddMovie2';
 
 class AddMovie extends React.Component {
