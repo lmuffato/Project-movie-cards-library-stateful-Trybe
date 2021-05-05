1e19b2f27ae3fc7908ff1afb3ddc40734bcdf5f9
Robertson <robertson.nunes@gmail.com>
2021-05-05 12:52:34 -0300

    Requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,12 @@
 // implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+      <form />
+    );
+  }
+}
+
+export default AddMovie;
