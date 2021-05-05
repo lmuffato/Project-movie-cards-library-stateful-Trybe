200ad32abd426c8f9ad8696fe153574ad791274d
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-05-05 14:45:54 -0300

    hello world

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,33 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+    };
+  }
+
+  render() {
+    const { input } = this.state;
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar
+          searchText={ input }
+          onSearchTextChange={ (event) => event.target.value }
+          bookmarkedOnly
+          onBookmarkedChange
+        />
+        <MovieList movies={ this.props.movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,14 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const { searchText, onSearchTextChange } = this.props;
+    return (
+      <div>
+        <input onChange={ onSearchTextChange } type="text" />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
