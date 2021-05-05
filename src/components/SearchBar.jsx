5a4465254a3fd6d39ca16dae1e22b39fad80e3b4
André Jaques Batista <andrejaques.b@gmail.com>
2021-05-05 08:38:57 -0300

    add require 04

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,13 +12,27 @@ export default function SearchBar(props) {
 
   return (
     <form className='form-container' data-testid='search-bar-form'>
-      <label className='label-container'>
+      <label data-testid='text-input-label'>
+        Inclui o texto:
+
         <input
           type='text'
           value={searchText}
           onChange={onSearchTextChange}
           data-testid='text-input'
         />
+
+      </label>
+      <label data-testid='checkbox-input-label'>
+        Mostrar somente favoritos
+
+        <input
+          type='checkbox'
+          checked={bookmarkedOnly}
+          onChange={onBookmarkedChange}
+          data-testid='checkbox-input'
+        />
+
       </label>
     </form>
   );
