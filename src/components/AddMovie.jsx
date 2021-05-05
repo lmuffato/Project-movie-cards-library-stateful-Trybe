b0277d75efb54978269562238753915cba9cae2b
rogerio <rogerio.cl@gmail.com>
2021-05-05 10:27:46 -0300

    mais uma chance

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import data from './data';
+import data from './dataComponents';
 
 class AddMovie extends Component {
   constructor() {
