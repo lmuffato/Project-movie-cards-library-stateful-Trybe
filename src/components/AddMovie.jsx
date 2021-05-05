486cc14aef44cca0fee339ab7a35bc3d91163f14
Debora Passos <deborinhapassos@gmail.com>
2021-05-05 19:25:46 -0300

    Requisitos 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,25 @@
-// implement AddMovie component here
+import React from 'react';
+
+export default class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      // subtitle: '',
+      // title: '',
+      // imagePath: '',
+      // storyline: '',
+      // rating: 0,
+      // genre: 'action',
+    };
+  }
+
+  render() {
+    // const { onClick } = this.props;
+    return (
+      <section>
+        <form data-testid="add-movie-form" />
+      </section>
+    );
+  }
+}
