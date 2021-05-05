4f95c8f04488442fbf1a28f0d500a93c49b0f28c
André Jaques Batista <andrejaques.b@gmail.com>
2021-05-05 14:18:29 -0300

    fixing lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
+
 export default function SearchBar(props) {
   const {
     searchText,
@@ -12,37 +13,36 @@ export default function SearchBar(props) {
 
   return (
     <form className="form-container" data-testid="search-bar-form">
-      
-        <label data-testid="text-input-label" htmlFor="text">
-          Inclui o texto:
-        </label>
+      <label data-testid="text-input-label" htmlFor="searchText">
+        Inclui o texto:
         <input
-          id="text"
+          id="searchText"
           type="text"
-          value={searchText}
-          onChange={onSearchTextChange}
+          value={ searchText }
+          onChange={ onSearchTextChange }
           data-testid="text-input"
         />
+      </label>
 
-      
-        <label data-testid="checkbox-input-label">
-          Mostrar somente favoritos
-          <input
-            type="checkbox"
-            checked={bookmarkedOnly}
-            onChange={onBookmarkedChange}
-            data-testid="checkbox-input"
-          />
-        </label>
+      <label data-testid="checkbox-input-label" htmlFor="bookmarkedOnly">
+        Mostrar somente favoritos
+        <input
+          id="bookmarkedOnly"
+          type="checkbox"
+          checked={ bookmarkedOnly }
+          onChange={ onBookmarkedChange }
+          data-testid="checkbox-input"
+        />
+      </label>
     </form>
   );
 }
 
 SearchBar.propTypes = {
-  searchText: PropTypes.string,
-  onSearchTextChange: PropTypes.func,
-  bookmarkedOnly: PropTypes.bool,
-  onBookmarkedChange: PropTypes.func,
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
   /* selectedGenre: PropTypes.string,
   onSelectedGenreChange: PropTypes.func, */
 };
