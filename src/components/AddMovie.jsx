02a5751988a23ba3d0b3f06f40a89d7ae7541175
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-05-06 12:10:20 -0300

    addmovie adicionado

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,10 @@
-// implement AddMovie component here
+import React from 'react';
+
+export default class AddMovie extends React.Component {
+  render() {
+    return (
+      <form>
+      </form>
+    );
+  }
+}
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -6,6 +6,7 @@ export default class MovieLibrary extends React.Component {
     return (
       <div>
         <SearchBar />
+        <AddMovie />
       </div>
     );
   }
