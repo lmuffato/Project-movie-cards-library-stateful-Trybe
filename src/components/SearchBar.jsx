230a897c12c0cb3b75a5df9a9b8ed27486a7b66d
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-05-06 15:46:46 -0300

    First and Second requirements done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,15 @@
-// implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <form data-testId="search-bar-form">
+        <fieldset>
+          <legend>My form</legend>
+        </fieldset>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
