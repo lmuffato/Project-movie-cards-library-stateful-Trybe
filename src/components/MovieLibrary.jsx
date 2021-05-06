921b2ac2d4265f05d6f8aa2f03ff579d3b329bc7
Nathalia Zebral <natzebral@gmail.com>
2021-05-06 13:15:36 -0300

    tentativa requisitos 1 a 5 - passando nos testes localmente

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,6 @@
 // implement AddMovie component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -16,10 +16,38 @@ class SearchBar extends React.Component {
           <label htmlFor="text-input" data-testid="text-input-label">
             Inclui o texto:
             <input
-              value="searchText"
+              value={ searchText }
               data-testid="text-input"
-              onChange="onSearchTextChange"
+              onChange={ onSearchTextChange }
             />
+          </label>
+          <label
+            data-testid="checkbox-input-label"
+            htmlFor="checkbox-input"
+          >
+            Mostrar somente favoritos
+            <input
+              type="checkbox"
+              checked={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
+              data-testid="checkbox-input"
+            />
+          </label>
+          <label
+            data-testid="select-input-label"
+            htmlFor="select-input"
+          >
+            Filtrar por gênero
+            <select
+              data-testid="select-input"
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+            >
+              <option data-testid="select-option" value="">Todos</option>
+              <option data-testid="select-option" value="action">Ação</option>
+              <option data-testid="select-option" value="comedy">Comédia</option>
+              <option data-testid="select-option" value="thriller">Suspense</option>
+            </select>
           </label>
         </form>
       </section>
