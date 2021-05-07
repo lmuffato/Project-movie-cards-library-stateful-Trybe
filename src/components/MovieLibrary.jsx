a06ee937bbc3bb52d746fad17c484de86f0347ff
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-05-07 12:02:25 -0300

    Requisito 15: Criaçao do Componente MovieLibrary - Por enquanto vazio

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,10 @@
 // implement AddMovie component here
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return 'Apenas para passar no requisito 15';
+  }
+}
+
+export default MovieLibrary;
