8abc9334e006a06e2005436161a20721f6583777
ThalitaC <thalitaceci@gmail.com>
2021-05-07 18:08:59 -0300

    Requisito 4: Renderize um input do tipo checkbox dentro do formulário em <SearchBar />

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,16 +4,28 @@ import PropTypes from 'prop-types';
 class SearchBar extends React.Component {
   render() {
     const { searchText,
-      onSearchTextChange } = this.props;
+      onSearchTextChange,
+      onBookmarkedChange,
+      bookmarkedOnly, } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label htmlFor="inclui-texto" data-testid="text-input-label">
+        <label htmlFor="include-text" data-testid="text-input-label">
           Inclui o texto:
           <input
             data-testid="text-input"
             type="text"
             value={ searchText }
             onChange={ onSearchTextChange }
+          />
+        </label>
+        <label htmlFor="show-favorites" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            data-testid="checkbox-input"
+            type="checkbox"
+            value={ searchText }
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
           />
         </label>
       </form>
@@ -24,6 +36,8 @@ class SearchBar extends React.Component {
 SearchBar.propTypes = {
   searchText: PropTypes.string.isRequired,
   onSearchTextChange: PropTypes.string.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
 };
 
 export default SearchBar;
