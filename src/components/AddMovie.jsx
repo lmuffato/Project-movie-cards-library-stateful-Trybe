1e6c9aae229c18b077dd35a68455c79a0fffd87c
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-05-07 17:33:00 -0300

    Seventh requirement done

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -19,7 +19,7 @@ class AddMovie extends React.Component {
     const { onClick } = this.props;
 
     return (
-      <form>
+      <form data-testid="add-movie-form">
         <input type="text" onClick={ onClick } />
       </form>
     );
