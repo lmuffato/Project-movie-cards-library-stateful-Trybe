5ce4980f075003485e30b5a160825ce764ec1dd7
carlos25xrm <carlos25xrm@gmail.com>
2021-05-07 03:00:56 -0300

    Requisito 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 class AddMovie extends React.Component {
   render() {
-    return ('oi');
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="inputDoTitulo">
+          <input />
+        </label>
+      </form>
+    );
   }
 }
 
