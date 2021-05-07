29efe2c655cad319b386ef5bbfff89641b3ef39b
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-05-07 10:13:43 -0300

    requisito 7 feito

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 export default class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+      
+  }
   render() {
     return (
-      <form />
+      <form data-testid="add-movie-form" />
     );
   }
 }
