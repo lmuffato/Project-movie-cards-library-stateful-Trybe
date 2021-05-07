31661f914acb7e5c4dc664169877f42db92693fd
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-05-07 11:28:14 -0300

    Enviando os primeiros requisitos

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,44 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+    //   bookMarkedOnly,
+    //   onBookMarkedChange,
+    //   selectdGenre,
+    //   onSelectedGenreChange,
+    } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="text1" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            type="text"
+            onChange={ onSearchTextChange }
+            value={ searchText }
+            id="text1"
+            data-testid="text-input"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
