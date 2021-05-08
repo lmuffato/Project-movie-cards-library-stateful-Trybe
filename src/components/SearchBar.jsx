f08a64ce01d277dd6cb59c8dfc4938753e7c634c
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-05-07 21:15:49 -0300

    last chance to lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import SearchBarInput from './SearchBarInput';
-import SearchBarSelect from './SearchBarSelect';
+import SearchBarInput from './SearchBarInput.js';
+import SearchBarSelect from './SearchBarSelect.js';
 
 class SearchBar extends React.Component {
   render() {
