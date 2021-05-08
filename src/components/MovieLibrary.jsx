ee969e077cec771b50a32958ae1e373c2009bd02
Lucas Pedroso <lepsk8@gmail.com>
2021-05-08 19:51:05 -0300

    feat: :sparkles: Add MovieLibrary.jsx

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,23 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
