c9cce01cbd71090bf8d81705bf535f57519af20a
Lucas Pedroso <lepsk8@gmail.com>
2021-05-08 19:47:09 -0300

    feat: :sparkles: add AddMovie.jsx

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,15 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  render() {
+    return (
+      <div>
+        <form action="">
+          
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
