f3e95a5284884456c818a7498422c597fe739f64
William <w.willers@outlook.com>
2021-05-07 22:08:32 -0400

    Iniciando implementacao AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,77 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+
+    this.setValueState = this.setValueState.bind(this);
+
+    // this.state = {
+    //   subtitle: '',
+    //   title: '',
+    //   imagePath: '',
+    //   storyline: '',
+    //   rating: 0,
+    //   genre: 'action',
+    // };
+  }
+
+  setValueState({ target }) {
+    const { value } = target.value;
+    const { name } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    // const { onClick } = this.props;
+    return (
+    // Implementar novo component para o form
+      <form data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.setValueState }
+          />
+        </label>
+        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            value={ subtitle }
+            data-testid="subtitle-input"
+            onChange={ this.setValueState }
+          />
+        </label>
+        <label htmlFor="image-input" data-testid="image-input-label">
+          Imagem
+          <input
+            type="text"
+            value={ imagePath }
+            data-testid="image-input"
+            onChange={ this.setValueState }
+          />
+        </label>
+        <label htmlFor="storyline-input" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            value={ storyline }
+            data-testid="storyline-input"
+            onChange={ this.setValueState }
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+// AddMovie.propTypes = {
+//   onClick: PropTypes.func.isRequired,
+// };
+
+export default AddMovie;
