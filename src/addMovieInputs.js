7b10717d07977fc2f7499294d1c76d31d0b59fb0
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-05-08 22:29:39 -0300

    Eighth requirement done

---
+++ src/addMovieInputs.js
@@ -0,0 +1,13 @@
+const inputs = [
+  {
+    label: 'title-input-label',
+    text: 'Título',
+    input: 'title-input',
+    name: 'title',
+    type: 'text',
+    // value: 'title',
+    // onChange: this.getProps().onSearchTextChange,
+  },
+];
+
+export default inputs;
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Input from './Input';
+import inputs from '../addMovieInputs';
 
 class AddMovie extends React.Component {
   constructor() {
     super();
+
+    this.onChangeHandle = this.onChangeHandle.bind(this);
+    this.getStates = this.getStates.bind(this);
 
     this.state = {
       title: '',
@@ -15,12 +20,51 @@ class AddMovie extends React.Component {
     };
   }
 
+  onChangeHandle({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  getStates() {
+    const {
+      title,
+      subtitle,
+      storyline,
+      imagePath,
+      rating,
+      genre,
+    } = this.state;
+
+    return {
+      title,
+      subtitle,
+      storyline,
+      imagePath,
+      rating,
+      genre,
+    };
+  }
+
   render() {
-    const { onClick } = this.props;
+    // const { onClick } = this.props;
 
     return (
       <form data-testid="add-movie-form">
-        <input type="text" onClick={ onClick } />
+        {inputs.map((input) => {
+          // https://stackoverflow.com/questions/35939289/how-to-destructure-into-dynamically-named-variables-in-es6
+          const { [input.name]: name } = this.getStates();
+
+          return (<Input
+            key={ input.input }
+            { ...input }
+            value={ name }
+            onChange={ this.onChangeHandle }
+          />);
+        })}
       </form>
     );
   }
