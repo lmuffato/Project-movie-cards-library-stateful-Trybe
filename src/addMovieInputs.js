11cad1a007979584b31cb840bf27d0f824873ff8
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-05-09 12:31:02 -0300

    Eleventh requirement done

--- src/addMovieInputs.js
+++ src/addMovieInputs.js
@@ -1,24 +1,31 @@
 const inputs = [
   {
+    CustomTag: 'input',
     label: 'title-input-label',
     text: 'Título',
     input: 'title-input',
     name: 'title',
-    type: 'text',
   },
   {
+    CustomTag: 'input',
     label: 'subtitle-input-label',
     text: 'Subtítulo',
     input: 'subtitle-input',
     name: 'subtitle',
-    type: 'text',
   },
   {
+    CustomTag: 'input',
     label: 'image-input-label',
     text: 'Imagem',
     input: 'image-input',
     name: 'imagePath',
-    type: 'text',
+  },
+  {
+    CustomTag: 'textarea',
+    label: 'storyline-input-label',
+    text: 'Sinopse',
+    input: 'storyline-input',
+    name: 'storyline',
   },
 ];
 
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 class Input extends React.Component {
   render() {
     const {
+      CustomTag,
       label,
       text,
       input,
@@ -17,7 +18,7 @@ class Input extends React.Component {
     return (
       <label htmlFor={ input } data-testid={ label }>
         {text}
-        <input
+        <CustomTag
           data-testid={ input }
           name={ name }
           type={ type }
@@ -31,11 +32,12 @@ class Input extends React.Component {
 }
 
 Input.propTypes = {
+  CustomTag: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
   input: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
+  type: PropTypes.string,
   value: PropTypes.string,
   checked: PropTypes.bool,
   onChange: PropTypes.func.isRequired,
@@ -44,6 +46,7 @@ Input.propTypes = {
 Input.defaultProps = {
   checked: false,
   value: '',
+  type: 'text',
 };
 
 export default Input;
