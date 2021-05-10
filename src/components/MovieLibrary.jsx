9e518a9459583d8f6112b630f773483db4f4e0ca
Samuel Melo <samuelmmjunior@gmail.com>
2021-05-10 19:37:11 -0300

    requisito 1 e 2

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,32 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      SearchText:'',
+      bookmarkedOnly:false,
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar 
+        SearchText={ this.state.SearchText } 
+        onSearchTextChange={ (event) => this.setState({ SearchText: event.target.value }) } 
+        onBookmarkedChange={ (event) => this.setState({ bookmarkedOnly: event.target.checked })}
+        />
+        <MovieList movies={ this.props.movies } />
+        {/* <AddMovie /> */}
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-function SearchBar({ 
-  searchText,
-  onSearchTextChange,
-  bookmarkedOnly,
-  onBookmarkedChange,
-  selectedGenre,
-  onSelectedGenreChange,props 
-}) {
-  
+class SearchBar extends Component {
+  render() {
+    // const eventTarget = (event) => event.target.value;
+    const { 
+    SearchText, 
+    onSearchTextChange, 
+    bookmarkedOnly, 
+    onBookmarkedChange,
+    selectedGenre, 
+    onSelectedGenreChange  
+    } = this.props
+
+    console.log(bookmarkedOnly);
+    return (
+      <form data-testid="search-bar-form">
+        <input value={ SearchText } onChange={ onSearchTextChange } type="text"  />
+        <input type="checkbox" checked={ bookmarkedOnly } onChange={ onBookmarkedChange } />
+      </form>
+      
+    )
+  }
 }
 
 export default SearchBar;
