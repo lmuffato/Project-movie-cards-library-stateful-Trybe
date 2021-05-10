aecfcc3d74e89f7e87a225aa7568710b6df828b1
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-05-10 17:39:52 -0300

    requisito 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -7,22 +7,39 @@ import AddMovie from './AddMovie';
 class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
+    const { movies } = this.props;
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies: props.movies,
+      movies: { movies },
     };
   }
 
+  searchedTextChange = (event) => {
+    this.setState({ searchText: event.target.value });
+  }
+
+  selectedGenreChange = (event) => {
+    this.setState({ selectedGenre: event.target.value });
+  }
+
+  bookmarkedChange = (event) => {
+    this.setState({ bookmarkedOnly: event.target.checked });
+  }
+
+  // fonte: https://stackoverflow.com/questions/43602665/uncheck-checkbox-programmatically-in-reactjs
   render() {
     const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
       <div>
         <SearchBar
           searchText={ searchText }
+          onSearchTextChange={ this.searchedTextChange }
           bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.bookmarkedChange }
           selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.selectedGenreChange }
         />
         <MovieList movies={ movies } />
         <AddMovie />
