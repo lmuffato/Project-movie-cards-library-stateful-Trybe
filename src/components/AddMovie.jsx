fcf4fe8ef015e24efe03b69fba01367cdcd3e703
diegoCorreia <diegomethal@gmail.com>
2021-05-10 17:54:20 -0300

    requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
 // implement AddMovie component here
+
+class AddMovie extends React.Component {
+  render(){
+    return (
+      <div>oi</div>
+    )
+  }
+}
+
+export default AddMovie;
