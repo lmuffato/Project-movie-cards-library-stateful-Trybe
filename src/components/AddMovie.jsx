49d4ed6b517595c5bc2cd5c4a325ebecb99fd2dd
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-05-10 12:12:00 -0300

    sexto e setimo requisitos

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,28 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: '',
+      genre: ''
+    }
+  }
+  render() {
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state
+    return (
+      <form data-testid="add-movie-form">
+        
+
+      </form>
+    )
+  }
+}
+
+export default AddMovie
