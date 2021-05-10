3031f7e16d0299a6e911462bba7d61bffbc7360e
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-05-10 10:19:09 -0300

    primeiro e segundo requisito sem lint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import SearchBar from './components/SearchBar';
+import MovieList from './components/MovieList';
+import movies from './data'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar label = 'Inclui o texto:'/>
+      <MovieList movies = {movies}/>
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,28 @@
-// implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+
+    const {
+      label, 
+      searchText, 
+      onSearchTextChange, 
+      bookmarkekOnly, 
+      onBookmarkedChange, 
+      selectGenre, 
+      onSelectedGenreChange} = this.props
+    
+    return (
+      <form data-testid="search-bar-form">
+        {label}
+        <br></br>
+        <input
+        value = {searchText}
+        onChange = {onSearchTextChange}
+        />
+      </form>
+    ) 
+  }
+}
+
+export default SearchBar
