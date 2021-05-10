d34fe5cdbbdc6f6375a2fcbc5066c712ccecdcc6
Luiz Otavio <luiz.otavio273@gmail.com>
2021-05-10 18:16:43 -0300

    Iniciando Projeto

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,11 @@
-// implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <h1>Iniciando Projeto</h1>
+    );
+  }
+}
+
+export default SearchBar;
