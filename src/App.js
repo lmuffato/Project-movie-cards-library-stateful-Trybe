ff45d7455f4e4d4de96f1d70a29fa2e6657fed0f
GetulioGutemberg <getulio.dev@gmail.com>
2021-05-11 17:40:19 -0300

    SearchBar in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import Header from './components/Header';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import "./App.css";
+import SearchBar from "./components/SearchBar";
 
 function App() {
+  // const movies = {
+  //     searchText: 'string',
+  //     onSearchTextChange: 'callback',
+  //     bookmarkedOnly: false,
+  //     onBookmarkedChange: 'callback',
+  //     selectedGenre: 'string',
+  //     onSelectedGenreChange: 'callback',
+  // }
+
   return (
     <div className="App">
       <Header />
+      <SearchBar
+        searchText="string"
+        onSearchTextChange="callback"
+        bookmarkedOnly="false"
+        onBookmarkedChange="callback"
+        selectedGenre="string"
+        onSelectedGenreChange='callback'
+      />
     </div>
   );
 }
