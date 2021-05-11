6a3ddb854d6ad3355c792954429589d9d4b0acf6
diegoCorreia <diegomethal@gmail.com>
2021-05-10 22:15:47 -0300

    requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,23 +2,41 @@ import React from 'react';
 // implement AddMovie component here
 
 class AddMovie extends React.Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     subtitle: '',
-  //     title: '',
-  //     imagePath: '',
-  //     storyline: '',
-  //     rating: 0,
-  //     genre: 'action',
-  //   };
-  // }
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    // const { title } = this.state;
+
+    this.state = {
+      // subtitle: '',
+      title: '',
+      // imagePath: '',
+      // storyline: '',
+      // rating: 0,
+      // genre: 'action',
+    };
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      title: target.value,
+    });
+  }
+  
   render() {
     return (
       <div>
         <form data-testid="add-movie-form">
-          <label htmlFor="text">
-            <input type="text" />
+          <label htmlFor="text" data-testid="title-input-label">
+            Título
+            <input
+              type="text"
+              data-testid="title-input"
+              value={ this.state.title }
+              onChange={ this.handleChange }
+            />
           </label>
         </form>
       </div>
--- src/tests/AddMovie.test.js
+++ src/tests/AddMovie.test.js
@@ -59,13 +59,13 @@ describe('6 - Crie um componente chamado `<AddMovie />`', () => {
   });
 });
 
-describe.only('7 - Renderize um formulário dentro de `<AddMovie />`', () => {
+describe('7 - Renderize um formulário dentro de `<AddMovie />`', () => {
   it('Renderize 1, e apenas 1, form', () => {
     expect(form).toHaveLength(1);
   });
 });
 
-describe('8 - Renderize um input do tipo texto dentro do formulário em `<AddMovie />` para obter o título do novo filme', () => {
+describe.only('8 - Renderize um input do tipo texto dentro do formulário em `<AddMovie />` para obter o título do novo filme', () => {
   it('Renderize um input de texto para quem usa escrever o titulo do filme', () => {
     expect(titleInput).toBeInTheDocument();
   });
