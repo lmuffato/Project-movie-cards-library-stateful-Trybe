e09bf34b677e408b0f4da1906ad44124b5b92db0
diegoCorreia <diegomethal@gmail.com>
2021-05-10 21:11:17 -0300

    requisito 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,23 +2,24 @@ import React from 'react';
 // implement AddMovie component here
 
 class AddMovie extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      subtitle: '',
-      title: '',
-      imagePath: '',
-      storyline: '',
-      rating: 0,
-      genre: 'action',
-    };
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     subtitle: '',
+  //     title: '',
+  //     imagePath: '',
+  //     storyline: '',
+  //     rating: 0,
+  //     genre: 'action',
+  //   };
+  // }
   render() {
     return (
       <div>
         <form data-testid="add-movie-form">
-          <input type="text" />
-          <button onClick={this.props.onClick}></button>
+          <label htmlFor="text">
+            <input type="text" />
+          </label>
         </form>
       </div>
     );
