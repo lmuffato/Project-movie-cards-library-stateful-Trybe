ddde8198c9c399aa0e3fcb8f704547601bcf6fa6
Samuel Melo <samuelmmjunior@gmail.com>
2021-05-11 11:42:15 -0300

    lint continua 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,26 +9,31 @@ class SearchBar extends Component {
       bookmarkedOnly,
       onBookmarkedChange,
       selectedGenre,
-      onSelectedGenreChange
+      onSelectedGenreChange,
     } = this.props;
 
     console.log(bookmarkedOnly);
     return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label"> 
-        Inclui o texto:<br />
-          <input data-testid="text-input" value={searchText} 
-          onChange={onSearchTextChange} type="text" />
-        </label><br />
-        <label data-testid="checkbox-input-label"> 
+          <label data-testid="text-input-label">
+        Inclui o texto:
+          <input 
+          data-testid="text-input" value={ searchText } 
+          onChange={ onSearchTextChange } type="text" 
+          />
+          </label>
+          <label data-testid="checkbox-input-label"> 
         Mostrar somente favoritos
-          <input data-testid="checkbox-input" type="checkbox" 
-          checked={bookmarkedOnly} onChange={onBookmarkedChange} />
-        </label><br />
-        <label data-testid="select-input-label"> 
+          <input 
+          data-testid="checkbox-input" type="checkbox" 
+          checked={ bookmarkedOnly } onChange={ onBookmarkedChange } 
+          />
+          </label>
+          <label data-testid="select-input-label"> 
         Filtrar por gênero
-          <select data-testid="select-input" value={selectedGenre} 
-          onChange={onSelectedGenreChange}>
+          <select 
+          data-testid="select-input" value={ selectedGenre } 
+          onChange={ onSelectedGenreChange }>
             <option data-testid="select-option" value="">Todos</option>
             <option data-testid="select-option" value="action">Ação</option>
             <option data-testid="select-option" value="comedy">Comédia</option>
