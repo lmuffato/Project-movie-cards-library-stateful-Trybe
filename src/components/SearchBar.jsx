5b9eecf3850917519a0386ebd252348f574636fa
GetulioGutemberg <getulio.dev@gmail.com>
2021-05-11 17:40:56 -0300

    Create SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,44 @@
 // implement AddMovie component here
+/* ### 1 - Crie um componente chamado `<SearchBar />`
+
+Esse componente renderizará uma barra com filtros acima da listagem de cartões. Quais cartões serão mostrados no componente `<MovieList />` dependerá dos filtros escolhidos. `<SearchBar />` deve receber como props:
+
+  - `searchText`, uma string
+  - `onSearchTextChange`, uma callback
+  - `bookmarkedOnly`, um boolean
+  - `onBookmarkedChange`, uma callback
+  - `selectedGenre`, uma string
+  - `onSelectedGenreChange`, uma callback
+
+**O que será verificado:**
+
+  - Será validado se o componente `<SearchBar />`, recebendo as devidas props, renderiza com sucesso */
+
+// import React from "react";
+// // import PropTypes from 'prop-types'
+
+import React from 'react';
+
+
+class SearchBar extends React.Component {
+  render(){
+    // const props = {
+    //   searchText='string',
+    //   onSearchTextChange= callback,
+    //   bookmarkedOnly=boolean,
+    //   onBookmarkedChange=callback,
+    //   selectedGenre='string',
+    //   onSelectedGenreChange=callback,
+    // }
+
+    // console.log(this.props.altura)
+    // const saida = this.props.altura == '1'? 'um' : 'nao é um'
+    return (
+      <div>
+        Funcionou o SearchBar!!! Eba!!!
+      </div>
+    )
+  }
+}
+
+export default SearchBar;
