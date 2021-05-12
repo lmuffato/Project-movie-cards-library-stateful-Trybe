f1a7e74a3dca7fdca94c91e02732256d36cb61c7
Luiz Otavio <luiz.otavio273@gmail.com>
2021-05-12 12:04:10 -0300

    Requisito 5 - Renderiza um select dentro do formulario

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -19,6 +19,14 @@ class SearchBar extends React.Component {
           <label htmlFor="Mostrar somente favoritos" data-testid="checkbox-input-label">
             <input type="checkbox" id="Mostrar somente favoritos" checked={ bookmarkedOnly } onChange={ onBookmarkedChange } data-testid="checkbox-input" />
           </label>
+          <label htmlFor="Filtrar por gênero" data-testid="select-input-label">
+            <select id="Filtrar por gênero" value={ selectedGenre } onChange={ onSelectedGenreChange } data-testid="select-input">
+              <option value="" data-testid="select-option">Todos</option>
+              <option value="action" data-testid="select-option">Ação</option>
+              <option value="comedy" data-testid="select-option">Comédia</option>
+              <option value="thriller" data-testid="select-option">Suspense</option>
+            </select>
+          </label>
         </form>
       </div>
     );
