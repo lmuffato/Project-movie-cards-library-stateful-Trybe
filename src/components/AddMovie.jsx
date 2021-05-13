05f01a8ce24f9f327d33f8e76ac29ae7495fc9c9
GetulioGutemberg <getulio.dev@gmail.com>
2021-05-13 01:57:26 -0300

    resolve requisito 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,14 @@
 // implement AddMovie component here
+import React from "react";
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+        <form data-testid="add-movie-form" >
+
+        </form>
+    );
+  }
+}
+
+export default AddMovie;
