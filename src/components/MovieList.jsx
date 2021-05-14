349afb9546ea99d25d7a4e10728b0aa642fff392
Renata Andrade <reapminas@gmail.com>
2021-05-14 11:13:25 -0300

    Requisito 1 - Crie um componente chamado <SearchBar />

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import MovieCard from './MovieCard';
-
 class MovieList extends React.Component {
-  render() {
-    const { movies } = this.props;
 
+  render() {
     return (
-      <div data-testid="movie-list" className="movie-list">
-        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
-      </div>
+      <div></div>
     );
   }
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,21 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.func
+};
+
+export default SearchBar;
