9da4ef8b4c29b925845d0d9bb047e2cc5dfae512
GetulioGutemberg <getulio.dev@gmail.com>
2021-05-14 00:39:42 -0300

    resolve requisito 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -5,8 +5,7 @@ class AddMovie extends React.Component {
   constructor() {
     super();
     this.state = {
-      title: 'teste',
-      titleInput: '',
+      title: '',
       subtitle: '',
       imagePath: '',
       storyline: '',
@@ -23,16 +22,24 @@ class AddMovie extends React.Component {
   });
 
   render() {
-    const { title } = this.state;
+    const { title, subtitle } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label data-testid="title-input-label" htmlFor="inputText">
+        <label data-testid="title-input-label" htmlFor="inputTitle">
           Título
-          <input
+          <input defaultValue={ title }
             data-testid="title-input"
             type="text"
-            id="inputText"
-            // value={ title }
+            id="inputTitle"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label data-testid="subtitle-input-label" htmlFor="inputSub">
+          Subtítulo
+          <input defaultValue={ subtitle }
+            data-testid="subtitle-input"
+            type="text"
+            id="inputSub"
             onChange={ this.handleChange }
           />
         </label>
