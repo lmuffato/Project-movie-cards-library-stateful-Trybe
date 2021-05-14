cf464b5d22db295b3b336a626fbd8824f6be569c
GetulioGutemberg <getulio.dev@gmail.com>
2021-05-14 00:32:32 -0300

    resolve requisito 8

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import './App.css';
 import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
 
 class App extends React.Component {
   // constructor() {
@@ -36,6 +37,7 @@ class App extends React.Component {
           selectedGenre="string"
           onSelectedGenreChange="callback"
         />
+        <AddMovie />
       </div>
     );
   }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,8 +2,42 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: 'teste',
+      titleInput: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: '',
+    };
+  }
+
+  handleChange = (({ target }) => {
+    const { key, value } = target;
+    this.setState({
+      [key]: value,
+    });
+  });
+
   render() {
-    return <form data-testid="add-movie-form" />;
+    const { title } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="inputText">
+          Título
+          <input
+            data-testid="title-input"
+            type="text"
+            id="inputText"
+            // value={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
+    );
   }
 }
 
