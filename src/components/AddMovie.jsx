7b15a7da053783204c8572931e62584c1da2d94c
GetulioGutemberg <getulio.dev@gmail.com>
2021-05-14 01:24:21 -0300

    resolve requisito 13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -14,20 +14,21 @@ class AddMovie extends React.Component {
     };
   }
 
-  handleChange = (({ target }) => {
+  handleChange = ({ target }) => {
     const { key, value } = target;
     this.setState({
       [key]: value,
     });
-  });
+  };
 
   render() {
-    const { title, subtitle, imagePath, storyline } = this.state;
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label data-testid="title-input-label" htmlFor="inputTitle">
           Título
-          <input defaultValue={ title }
+          <input
+            defaultValue={ title }
             data-testid="title-input"
             type="text"
             id="inputTitle"
@@ -36,7 +37,8 @@ class AddMovie extends React.Component {
         </label>
         <label data-testid="subtitle-input-label" htmlFor="inputSub">
           Subtítulo
-          <input defaultValue={ subtitle }
+          <input
+            defaultValue={ subtitle }
             data-testid="subtitle-input"
             type="text"
             id="inputSub"
@@ -45,7 +47,8 @@ class AddMovie extends React.Component {
         </label>
         <label data-testid="image-input-label" htmlFor="inputImage">
           Imagem
-          <input defaultValue={ imagePath }
+          <input
+            defaultValue={ imagePath }
             data-testid="image-input"
             type="text"
             id="inputImage"
@@ -54,12 +57,56 @@ class AddMovie extends React.Component {
         </label>
         <label data-testid="storyline-input-label" htmlFor="inputStoryline">
           Sinopse
-          <input defaultValue={ storyline }
+          <input
+            defaultValue={ storyline }
             data-testid="storyline-input"
             type="text"
             id="inputStoryline"
             onChange={ this.handleChange }
           />
+        </label>
+        <label data-testid="rating-input-label" htmlFor="inputRating">
+          Avaliação
+          <input
+            defaultValue={ rating }
+            data-testid="rating-input"
+            type="number"
+            id="inputRating"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label data-testid="genre-input-label" htmlFor="inputRating">
+          Gênero
+          <select data-testid="genre-input">
+            {/* <option
+              defaultValue={ genre }
+              onChange={ this.handleChange }
+              data-testid="genre-option"
+            >
+              { genre }
+            </option> */}
+            <option
+              value="action"
+              onChange={ this.handleChange }
+              data-testid="genre-option"
+            >
+              Ação
+            </option>
+            <option
+              value="comedy"
+              onChange={ this.handleChange }
+              data-testid="genre-option"
+            >
+              Comédia
+            </option>
+            <option
+              value="thriller"
+              onChange={ this.handleChange }
+              data-testid="genre-option"
+            >
+              Suspense
+            </option>
+          </select>
         </label>
       </form>
     );
