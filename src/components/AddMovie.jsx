86af50d04d01604cc4a3742c641ecea3a36df792
Renata Andrade <reapminas@gmail.com>
2021-05-14 11:52:14 -0300

    Requisito 6 - Crie um componente chamado <AddMovie />

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,15 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section></section>
+    )
+  }
+}
+
+export default AddMovie;
