4e957121dd15b7a669a8417968b5cc31997f6843
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-05-14 14:30:05 -0300

    retornando ao est inicial onClick

---
+++ src/components/RatingInput.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class RatingInput extends React.Component {
+  render() {
+    const { rating, onChange } = this.props;
+    return (
+      <label
+        htmlFor="rating-input"
+        data-testid="rating-input-label"
+      >
+        Avaliação:
+        <input
+          value={ rating }
+          data-testid="rating-input"
+          onChange={ onChange }
+          type="number"
+          name="rating"
+        />
+      </label>
+    );
+  }
+}
+
+RatingInput.propTypes = {
+  rating: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default RatingInput;
