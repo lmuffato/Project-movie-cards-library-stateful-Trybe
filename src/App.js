d1c3119d9b4491d89d9928a67620d6610bd6b661
Renata Andrade <reapminas@gmail.com>
2021-05-16 19:28:52 -0300

    Requisito 12 e inserçao de estado requisito 8-12

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Header from './components/Header';
 import MovieLibrary from './components/MovieLibrary';
 import './App.css';
+import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MovieLibrary />
+      <AddMovie />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,36 +1,95 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleSubtitleChange = this.handleSubtitleChange.bind(this);
+    this.handleImagePathChange = this.handleImagePathChange.bind(this);
+    this.handleStorylineChange = this.handleStorylineChange.bind(this);
+    this.handleRatingChange = this.handleRatingChange.bind(this);
+    this.handleGenreChange = this.handleGenreChange.bind(this);
+  }
+
+  handleTitleChange(event) {
+    this.setState({
+      title: event.target.value,
+    });
+  }
+
+  handleSubtitleChange(event) {
+    this.setState({
+      subtitle: event.target.value,
+    });
+  }
+
+  handleImagePathChange(event) {
+    this.setState({
+      imagePath: event.target.value,
+    });
+  }
+
+  handleStorylineChange(event) {
+    this.setState({
+      storyline: event.target.value,
+    });
+  }
+
+  handleRatingChange(event) {
+    this.setState({
+      rating: event.target.value,
+    });
+  }
+
+  handleGenreChange(event) {
+    this.setState({
+      genre: event.target.value,
+    });
+  }
 
   render() {
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="title" data-testid="title-input-label">
           Título
           <br />
-          <input type="text" title="" data-testid="title-input" id="title" />
+          <input value={ title } type="text" data-testid="title-input" id="title" onChange={ this.handleTitleChange } />
         </label>
         <br />
         <label htmlFor="subtitle" data-testid="subtitle-input-label">
           Subtítulo
           <br />
-          <input type="text" subtitle="" data-testid="subtitle-input" id="subtitle" />
+          <input value={ subtitle } type="text" data-testid="subtitle-input" id="subtitle" onChange={ this.handleSubtitleChange } />
         </label>
         <br />
         <label htmlFor="image" data-testid="image-input-label">
           Imagem
           <br />
-          <input type="text" imagePath="" data-testid="image-input" id="image" />
+          <input value={ imagePath } type="text" data-testid="image-input" id="image" onChange={ this.handleImagePathChange } />
         </label>
         <br />
         <label htmlFor="sinopse" data-testid="storyline-input-label">
           Sinopse
           <br />
-          <textarea type="text" storyline="" data-testid="storyline-input" />
+          <textarea value={ storyline } type="text" data-testid="storyline-input" onChange={ this.handleStorylineChange } />
         </label>
+        <br />
+        <label htmlFor="rating" data-testid="rating-input-label">
+          Avaliação
+          <br />
+          <input value={ rating } type="number" data-testid="rating-input" id="rating" onChange={ this.handleRatingChange } />
+        </label>
+        <br />
       </form>
     );
   }
