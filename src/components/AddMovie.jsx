5e2e46bde2e0286a07563b419889a90482510c60
Luiz <luizfvaccarif@gmail.com>
2021-05-17 00:14:31 +0100

    Requirement 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,7 +4,7 @@ class AddMovie extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // subtitle: '',
+      subtitle: '',
       title: '',
       // imagePath: '',
       // storyline: '',
@@ -14,20 +14,32 @@ class AddMovie extends React.Component {
     this.handleState = this.handleState.bind(this);
   }
 
-  handleState(event) {
-    this.setState({ title: event.target.value });
+  handleState({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
   }
 
   render() {
-    const { title } = this.state;
+    const { title, subtitle } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label data-testid="title-input-label" htmlFor="title-input">
           Título
           <input
             type="text"
+            name="title"
             value={ title }
             data-testid="title-input"
+            onChange={ this.handleState }
+          />
+        </label>
+        <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
+          Subtítulo
+          <input
+            type="text"
+            name="subtitle"
+            value={ subtitle }
+            data-testid="subtitle-input"
             onChange={ this.handleState }
           />
         </label>
