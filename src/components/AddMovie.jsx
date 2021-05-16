c423888a96e68cfa2ff791c3a1279e15c162a896
Luiz <luizfvaccarif@gmail.com>
2021-05-16 23:52:22 +0100

    Requirement 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
-  /*  constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
-      subtitle: '',
+      // subtitle: '',
       title: '',
-      imagePath: '',
-      storyline: '',
-      rating: 0,
-      genre: 'action',
+      // imagePath: '',
+      // storyline: '',
+      // rating: 0,
+      // genre: 'action',
     };
-  } */
+    this.handleState = this.handleState.bind(this);
+  }
+
+  handleState(event) {
+    this.setState({ title: event.target.value });
+  }
 
   render() {
+    const { title } = this.state;
     return (
       <form data-testid="add-movie-form">
-        Form
+        <label data-testid="title-input-label" htmlFor="title-input">
+          Título
+          <input
+            type="text"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleState }
+          />
+        </label>
       </form>
     );
   }
