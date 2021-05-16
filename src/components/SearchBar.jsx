b7bafe83b5b528134d09cc53d4192f7423c8bd30
renancosta21 <renancosta21@gmail.com>
2021-05-15 22:56:47 -0300

    requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,34 +4,47 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
-    } = this.props;
+    const { searchText, onSearchTextChange } = this.props;
+    const { bookmarkedOnly, onBookmarkedChange } = this.props;
+    const { selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <div>
         <form data-testid="search-bar-form">
-          <fieldset>
-            <label htmlFor="labeIncluiOTexto" data-testid="text-input-label">
-              Inclui o texto:
-              <input
-                value={ searchText }
-                name="bookmarkedOnly"
-                type="text"
-                onChange={ onSearchTextChange }
-                data-testid="text-input"
-              />
-            </label>
-          </fieldset>
-          <fieldset>
-            <label htmlFor="labelMostrarFavoritos" data-testid="checkbox-input-label">
-              Mostrar somente favoritos
-              <input
-                type="checkbox"
-                data-testid="checkbox-input"
-                checked={ bookmarkedOnly }
-                onChange={ onBookmarkedChange }
-              />
-            </label>
-          </fieldset>
+          <label htmlFor="labeIncluiOTexto" data-testid="text-input-label">
+            Inclui o texto:
+            <input
+              value={ searchText }
+              name="bookmarkedOnly"
+              type="text"
+              onChange={ onSearchTextChange }
+              data-testid="text-input"
+            />
+          </label>
+          <br />
+          <label htmlFor="labelMostrarFavoritos" data-testid="checkbox-input-label">
+            Mostrar somente favoritos
+            <input
+              type="checkbox"
+              data-testid="checkbox-input"
+              checked={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
+            />
+          </label>
+          <br />
+          <label htmlFor="labelInputSelect" data-testid="select-input-label">
+            Filtrar por gênero
+            <select
+              type="select"
+              data-testid="select-input"
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+            >
+              <option data-testid="select-option" value="">Todos</option>
+              <option data-testid="select-option" value="action">Ação</option>
+              <option data-testid="select-option" value="comedy">Comédia</option>
+              <option data-testid="select-option" value="thriller">Suspense</option>
+            </select>
+          </label>
         </form>
       </div>
     );
