85d13a8e3b57b236249bc94982a0270063234600
biazidioti <bzferreira.bio@gmail.com>
2021-05-18 11:39:12 -0300

    Seilamais oq ta rolando.!

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import Header from './components/Header';
 import './App.css';
 import MovieLibrary from './components/MovieLibrary';
 import data from './data';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MovieLibrary moviesMly={ data } />
-      <SearchBar />
-      <AddMovie />
     </div>
   );
 }
