a3d0acca2e866f7e0ff0095ba42d3cfbbbb6ba9d
Tiago Santos <tiago881@gmail.com>
2021-05-20 10:46:10 -0300

    18 - Renderize <MovieList /> dentro de <MovieLibrary />

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,23 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import MovieCard from './MovieCard';
+import SearchBar from './SearchBar';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props;
+    const { moviesConst } = this.props;
 
     return (
       <div data-testid="movie-list" className="movie-list">
-        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+        {moviesConst.map((movie) => (
+          <MovieCard key={ movie.title } movie={ movie } />
+        ))}
       </div>
     );
   }
 }
 
 MovieList.propTypes = {
-  movies: PropTypes.arrayOf(
-    PropTypes.object,
-  ).isRequired,
+  moviesConst: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default MovieList;
