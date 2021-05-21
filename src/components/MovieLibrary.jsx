2700b87fd0a76e2ddbb2a35c2c81c4d5cedba4b6
Renata Andrade <reapminas@gmail.com>
2021-05-21 18:41:33 -0300

    Requisito 17 - Renderize <SearchBar /> dentro de <MovieLibrary />

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,22 +1,56 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
+import MovieCard from './MovieCard';
 
 class MovieLibrary extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    console.log(movies);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+    console.log(this.state);
+  }
 
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    console.log(movies);
     return (
       <div>
+        {movies.map((movie) => (
+          <div key={ movie.title }>
+            <MovieCard movie={ movie } />
+          </div>
+        ))}
         <h2>My awesome movie library</h2>
-        <SearchBar />
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+        />
         {/* <MovieList movies={this.props.movies} /> */}
         <AddMovie handleMovieButton={ this.handleMovieButton } />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+    bookmarked: PropTypes.bool,
+    genre: PropTypes.string,
+  })).isRequired,
+};
 
 export default MovieLibrary;
