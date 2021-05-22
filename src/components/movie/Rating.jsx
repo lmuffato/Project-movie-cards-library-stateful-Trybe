ef7f7cd7fe70057af4ab883ed53ce08f7abe8c58
Roberto Natividade <robertonnv@gmail.com>
2021-05-22 20:53:27 -0300

    Requisito

--- src/components/movie/Rating.jsx
+++ src/components/movie/Rating.jsx
@@ -3,19 +3,24 @@ import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    const { rating } = this.props;
+    const { rating, handleRating } = this.props;
     return (
-      <div className="movie-card-rating" data-testid="rating">
-        <span className="rating">{rating}</span>
-      </div>
+      <label data-testid="rating-input-label" htmlFor="rating-input">
+        Avaliação
+        <input
+          type="number"
+          value={ rating }
+          data-testid="rating-input"
+          onChange={ handleRating }
+        />
+      </label>
     );
   }
 }
 
-Rating.propTypes = { rating: PropTypes.number };
-
-Rating.defaultProps = {
-  rating: 'undefined',
+Rating.propTypes = {
+  rating: PropTypes.string.isRequired,
+  handleRating: PropTypes.func.isRequired,
 };
 
 export default Rating;
