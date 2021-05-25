8c854de3156c8f056101eaecf421c2930175019b
pauloeliezerg <pauloeliezerg@gmail.com>
2021-05-25 15:57:14 -0300

    Added AddMovie component

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,31 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    return (
+      <form>
+        <textarea name="subtitle" />
+        <textarea name="title" />
+        <textarea name="imagePath" />
+        <textarea name="storyLine" />
+        <select name="rating">rating</select>
+        <select name="genre">genre</select>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
