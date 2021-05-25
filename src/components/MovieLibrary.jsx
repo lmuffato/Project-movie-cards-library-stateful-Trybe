05dab5881b283251cf3f38aaf9702e091263c814
biazidioti <bzferreira.bio@gmail.com>
2021-05-25 09:04:46 -0300

    Atualiza Status

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -9,25 +9,8 @@ class MovieLibrary extends Component {
   constructor(props) {
     super(props);
     const { moviesMly } = this.props;
-    this.state = {
-      searchText: '',
-      bookmarkedOnly: false,
-      selectedGenre: '',
-      movies: moviesMly };
+    this.state = { movies: moviesMly };
   }
-
-  onSearchTextChange = ({ target }) => {
-    this.setState({ searchText: target.value });
-  };
-
-  onBookmarkedChange = ({ target }) => {
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({ bookmarkedOnly: value });
-  };
-
-  onSelectedGenreChange = ({ target }) => {
-    this.setState({ selectedGenre: target.value });
-  };
 
   onClick = (addMovie) => {
     this.setState((previusState) => ({
@@ -36,25 +19,7 @@ class MovieLibrary extends Component {
   }
 
   render() {
-    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
-    let { movies } = this.state;
-    const filterMoviesByBookmarked = movies.filter((movie) => movie.bookmarked === true);
-    const filterMoviesByGenre = movies.filter((movie) => movie.genre === selectedGenre);
-
-    const filterMoviesByText = movies.filter((movie) => movie.title.includes(searchText)
-      || movie.subtitle.includes(searchText)
-      || movie.storyline.includes(searchText));
-
-    if (searchText) {
-      movies = filterMoviesByText;
-    }
-    if (bookmarkedOnly) {
-      movies = filterMoviesByBookmarked;
-    }
-    if (selectedGenre) {
-      movies = filterMoviesByGenre;
-    }
-
+    const { movies } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
