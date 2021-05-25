5d428dfa05bae820dc31872fa7ba14c1b9c772f2
jlfagundes <jlfagundes@gmail.com>
2021-05-25 12:58:00 -0300

    fix: (req1, req2, req3) eslint

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,21 +2,21 @@
 // Inicio do projeto jlfagundes
 import React from 'react';
 
-import MovieList from './MovieList';
+// import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={ this.props.movies } />
+        {/* <MovieList movies={ this.props.movies } /> */}
         <AddMovie />
       </div>
     );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,11 +2,19 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  
   render() {
     return (
       <form data-testid="search-bar-form">
-        <input type="text" />
+        <label htmlFor="input-text" data-testid="text-input-label">
+          Inlcui o texto:
+          <input
+            type="text"
+            data-testid="text-input"
+            id="input-text"
+            onChange="onSearchTextChange"
+            value="searchText"
+          />
+        </label>
       </form>
     );
   }
