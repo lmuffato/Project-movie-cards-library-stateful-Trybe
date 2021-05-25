3b02deecfd9424cf992228907633a112d289746f
Junior Portela <portelajr14@gmail.com>
2021-05-25 13:01:07 -0300

    feat: req14 ao req20 - movie library + search

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -10,8 +10,41 @@ class MovieLibrary extends Component {
     super(props);
     const { movies } = this.props;
     this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
       movies,
     };
+  }
+
+  handleSearching = (event) => {
+    this.setState({ searchText: event.target.value });
+  }
+
+  handleBookmarked = (event) => {
+    this.setState({ bookmarkedOnly: event.target.checked });
+  }
+
+  handleSelectedGenre = (event) => {
+    this.setState({ selectedGenre: event.target.value });
+  }
+
+  filtering = () => {
+    const { searchText, bookmarkedOnly, movies, selectedGenre } = this.state;
+    if (bookmarkedOnly) {
+      return movies.filter((movie) => movie.bookmarkedOnly === true);
+    }
+
+    if (selectedGenre !== '') {
+      return movies.filter((movie) => movie.genre === selectedGenre);
+    }
+
+    if (searchText !== '') {
+      return movies.filter((movie) => movie.title.includes(searchText))
+      || movies.subtitle.includes(searchText)
+      || movies.storyLine.includes(searchText);
+    }
+    return movies.filter((movie) => movie.title.includes(searchText));
   }
 
   addCardMovie = (cardMovie) => {
@@ -22,12 +55,19 @@ class MovieLibrary extends Component {
   }
 
   render() {
-    const { movies } = this.props;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    const movies = this.filtering();
 
     return (
       <div>
-        <h2> My awesome movie library </h2>
-        <SearchBar />
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.handleSearching }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.handleBookmarked }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.handleSelectedGenre }
+        />
         <MovieList movies={ movies } />
         <AddMovie onClick={ this.addCardMovie } />
       </div>
