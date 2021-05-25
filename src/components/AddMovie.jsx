d69045d22f91ced518b9787a47710631db93ba86
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-05-25 15:14:14 -0300

    requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,24 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+  render() {
+    const { onClick } = this.props;
+    return(
+      <p>teste</p>
+    );
+  }
+}
+
+export default AddMovie;
