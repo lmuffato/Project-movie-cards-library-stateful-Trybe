bd59ce29842ee43a033018c68069da4b43dbfd84
jlfagundes <jlfagundes@gmail.com>
2021-05-25 12:43:13 -0300

    Feat: initializing component SearchBar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,2 +1,26 @@
 // implement AddMovie component here
 // Inicio do projeto jlfagundes
+import React from 'react';
+
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={ this.props.movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,15 @@
 // implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+        <input type="text" />
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
