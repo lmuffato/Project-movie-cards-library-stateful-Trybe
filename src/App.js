df36d4e1e4d6564882bcf55babdcfea31cb45ed5
Wendrick <wendrick.b.c1@gmail.com>
2021-05-26 02:37:33 -0300

    Esqueci o prototype

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import './App.css';
 import AddMovie from './components/AddMovie';
-import MovieLibrary from './components/MovieLibrary';
+// import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
