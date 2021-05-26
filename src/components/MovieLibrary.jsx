b87e499114dd7c6b0261795b1f31ccd4ab1d7b19
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-05-25 23:24:00 -0300

    requisito 19

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,13 +5,29 @@ import AddMovie from './AddMovie';
 import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
     const { movies } = this.props;
+    this.state = {
+      movies,
+    };
+
+    this.addMovie = this.addMovie.bind(this);
+  }
+
+  addMovie(newMovie) {
+    this.setState((oldState) => ({
+      movies: [...oldState.movies, newMovie],
+    }));
+  }
+
+  render() {
+    const { movies } = this.state;
     return (
       <div>
         <SearchBar />
         <MovieList movies={ movies } />
-        <AddMovie />
+        <AddMovie onClick={ this.addMovie } />
       </div>
     );
   }
