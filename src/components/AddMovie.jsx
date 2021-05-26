ce58936408779e7a5add4eecb0024b5131f5d6e6
Arlesson Moura <arlessonmss@gmail.com>
2021-05-26 02:59:56 -0300

    Feat: 4° && 5° requirement?

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,22 @@
 // implement AddMovie component here !!!estado!!!
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: '',
+    }
+  }
+
+  render() {
+    return(
+
+    );
+  }
+}
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,12 +14,31 @@ class SearchBar extends Component {
     return (
       <form data-testid="search-bar-form">
         <label data-testid="text-input-label">
-          Inclui o texto:
+        Inclui o texto:
           <input
+            type="text"
             data-testid="text-input"
             value={ searchText }
             onClick={ onSearchTextChange }
           />
+        </label>
+        <label data-testid="checkbox-input-label">
+        Mostrar somente favoritos
+          <input
+            type="checkbox"
+            data-testid="checkbox-input"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+        </label>
+        <label data-testid="select-input-label">
+        Filtrar por gênero
+          <select data-testid="select-input" value={ selectedGenre } onChange={ onSelectedGenreChange }>
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
         </label>
       </form>
     );
