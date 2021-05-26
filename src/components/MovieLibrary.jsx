d1289cf32659fe2d439e346c6481bc61723ea790
GetulioGutemberg <getulio.dev@gmail.com>
2021-05-26 01:47:10 -0300

    resolve requisito 19

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,18 +5,61 @@ import AddMovie from './AddMovie';
 import MovieList from './MovieList';
 
 export default class MovieLibrary extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies };
+  }
+
+  onClick = (addMovie) => {
+    this.setState((previusState) => ({
+      movies: [...previusState.movies, addMovie],
+    }));
+  }
+
+  onBookmarkedChange = (event) => {
+    this.setState({ bookmarkedOnly: event.target.checked });
+  }
+
+  onSearchTextChange = (event) => {
+    this.setState({ searchText: event.target.value });
+  }
+
+  onSelectedGenreChange = (event) => {
+    this.setState({ selectedGenre: event.target.value });
+  }
 
   render() {
-    const { movies } = this.props;
+    const { movies, searchText, bookmarkedOnly, selectedGenre } = this.state;
+    const filterMovies = movies.filter(({ title, subtitle, storyline }) => (
+      title.toLowerCase().includes(searchText.toLowerCase())
+      || subtitle.toLowerCase().includes(searchText.toLowerCase())
+      || storyline.toLowerCase().includes(searchText.toLowerCase())
+    ));
+    const filterMoviesByBookmarked = filterMovies.filter(({ bookmarked }) => {
+      if (!bookmarkedOnly) {
+        return true;
+      }
+      return bookmarked === true;
+    });
+
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={ movies } />
-        <AddMovie onClick={ (teste) => console.log(teste) } />
+        <SearchBar
+          searchText={ searchText }
+          selectedGenre={ selectedGenre }
+          bookmarkedOnly={ bookmarkedOnly }
+          onSearchTextChange={ this.onSearchTextChange }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+        <MovieList movies={ filterMoviesByBookmarked } />
+        <AddMovie onClick={ this.onClick } />
       </div>
     );
   }
