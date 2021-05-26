9f5127ef9ca2d801eb25deed14e9d639424becfe
Wendrick <wendrick.b.c1@gmail.com>
2021-05-26 02:13:17 -0300

    Commit pra ver como vai indo

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,22 @@
-// implement AddMovie component here Ok
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
+    return (
+      <div>
+        <form data-testid="search-bar-form" />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
