cf282ad526df453ab37909a3508f3203abbef532
Wendrick <wendrick.b.c1@gmail.com>
2021-05-26 04:23:32 -0300

    Tentando o library

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -16,20 +16,56 @@ class MovieLibrary extends Component {
     };
   }
 
+  addMovie = (newMovie) => {
+    this.setState((lastState) => ({ movies: [...lastState.movies, newMovie] }));
+  }
+
+  searchMovies = () => {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    let currentFilter = movies;
+
+    if (bookmarkedOnly) {
+      return currentFilter.filter(({ bookmarked }) => bookmarked === true);
+    }
+
+    if (selectedGenre !== '') {
+      return currentFilter.filter(({ genre }) => genre === selectedGenre);
+    }
+
+    if (searchText !== '') {
+      const textToLowerCase = searchText.toLowerCase();
+      currentFilter = movies.filter(({ title, subtitle, storyline }) => (
+        title.toLowerCase().includes(textToLowerCase)
+        || subtitle.toLowerCase().includes(textToLowerCase)
+        || storyline.toLowerCase().includes(textToLowerCase)
+      ));
+    }
+
+    return currentFilter;
+  }
+
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={ this.props.movies } />
-        <AddMovie />
+        <MovieList movies={ this.searchMovies() } />
+        <AddMovie onClick={ (newMovie) => this.addMovie(newMovie) } />
       </div>
     );
   }
 }
 
-MovieLibrary.propTypes = ({
-  movies: PropTypes.arrayOf(PropTypes.object),
-}).isRequired;
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+    bookmarked: PropTypes.bool,
+    genre: PropTypes.string,
+  })).isRequired,
+};
 
 export default MovieLibrary;
